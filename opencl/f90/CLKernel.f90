e1ae86a8c5d45cd36ff409797554da542faea708
rasmussn <rasmussn@kirkelanlgov.local>
2010-11-05 14:10:23 -0600

    Added several explicit setKernelArgXXX methods

--- opencl/f90/CLKernel.f90
+++ opencl/f90/CLKernel.f90
@@ -13,9 +13,17 @@ module CLKernel_mod
       integer(c_int) :: elapsed        ! elapsed time in microseconds
    contains
       procedure, pass(this) :: init
-      procedure, pass(this) :: setKernelArg
+      procedure, pass(this) :: setKernelArgInt
+      procedure, pass(this) :: setKernelArgLoc
+      procedure, pass(this) :: setKernelArgMem
+      procedure, pass(this) :: setKernelArgReal
       procedure, pass(this) :: run
    end type CLKernel
+
+   interface setKernelArg
+      module procedure setKernelArgInt, setKernelArgMem, setKernelArgLoc, &
+                       setKernelArgReal
+   end interface
 
 contains
 
@@ -29,19 +37,20 @@ contains
       integer(c_size_t) :: lengths(1)
       type(c_ptr) :: source
 
+      integer(c_size_t) :: err_len
+      integer, parameter :: err_buf_len = 2048
+      character :: err_buf(err_buf_len)
+
       this%device = device
       this%commands = commands
       this%profiling = .false.
       this%elapsed = 0
 
-      print *, "CLKernel:init: ", filename, " name==", name
       status = CL_SUCCESS
 
       ! Create the compute program from the source buffer
       !
-      print *, "will load program ", filename
       source = load_program_source(filename, lengths)
-      print *, "did load program, count==", lengths(1)
 
       this%program = clCreateProgramWithSource(context, 1, source, lengths, status)
       call c_free(source)
@@ -54,11 +63,10 @@ contains
       !
       status = clBuildProgram(this%program, 0, C_NULL_PTR, C_NULL_CHAR, C_NULL_FUNPTR, C_NULL_PTR)
       if (status /= CL_SUCCESS) then
-!       size_t len;
-!       char buffer[2048];
 
          print *, "CLKernel::init: Failed to build program executable!"
-!         clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+         status = clGetProgramBuildInfo(this%program, this%device, &
+                                        CL_PROGRAM_BUILD_LOG, err_buf_len, err_buf, err_len)
 !         printf("%s\n", buffer);
          call stop_on_error(status)
       end if
@@ -72,21 +80,69 @@ contains
       end if
    end function init
 
-   function setKernelArg(this, argid, cl_mem_obj) result(status)
+   function setKernelArgInt(this, argid, int_var) result(status)
+      implicit none
+      class(CLKernel) :: this
+      integer, intent(in) :: argid
+      integer(c_int) :: int_var
+      integer(cl_int) :: status
+      integer(c_size_t) :: arg_size
+
+   ! TODO - FIXME - sizeof_int
+      arg_size = 4
+
+      status = clSetKernelArg(this%kernel, argid, arg_size, c_loc(int_var))
+      if (status /= CL_SUCCESS) then
+         print *, "CLDevice::setKernelArgInt: Failed to set kernel argument!"
+         call stop_on_error(status)
+      end if
+   end function setKernelArgInt
+
+   function setKernelArgLoc(this, argid, size) result(status)
+      implicit none
+      class(CLKernel) :: this
+      integer, intent(in) :: argid
+      integer(c_size_t) :: size
+      integer(cl_int) :: status
+
+      status = clSetKernelArg(this%kernel, argid, size, C_NULL_PTR)
+      if (status /= CL_SUCCESS) then
+         print *, "CLDevice::setKernelArgLoc: Failed to set kernel argument!"
+         call stop_on_error(status)
+      end if
+   end function setKernelArgLoc
+
+   function setKernelArgMem(this, argid, cl_mem_obj) result(status)
       implicit none
       class(CLKernel) :: this
       integer, intent(in) :: argid
       type(c_ptr) :: cl_mem_obj
       integer(cl_int) :: status
 
-      status = clSetKernelArg(this%kernel, argid, c_sizeof_cl_mem(), cl_mem_obj)
+      status = clSetKernelArg(this%kernel, argid, c_sizeof_cl_mem(), c_loc(cl_mem_obj))
       if (status /= CL_SUCCESS) then
-         print *, "CLDevice::setKernelArg: Failed to set kernel argument!"
+         print *, "CLDevice::setKernelArgMem: Failed to set kernel argument!"
          call stop_on_error(status)
       end if
+   end function setKernelArgMem
 
-   end function setKernelArg
+   function setKernelArgReal(this, argid, real_var) result(status)
+      implicit none
+      class(CLKernel) :: this
+      integer, intent(in) :: argid
+      real(c_float) :: real_var
+      integer(cl_int) :: status
+      integer(c_size_t) :: arg_size
 
+   ! TODO - FIXME - sizeof_int
+      arg_size = 4
+
+      status = clSetKernelArg(this%kernel, argid, arg_size, c_loc(real_var))
+      if (status /= CL_SUCCESS) then
+         print *, "CLDevice::setKernelArgReal: Failed to set kernel argument!"
+         call stop_on_error(status)
+      end if
+   end function setKernelArgReal
 
    function run(this, gWorkSizeX, gWorkSizeY, lWorkSizeX, lWorkSizeY) result(status)
       implicit none
