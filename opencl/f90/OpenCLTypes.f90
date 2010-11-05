2bb65e614145e5e3bd7541b5e3460605fd383c89
rasmussn <rasmussn@kirkelanlgov.local>
2010-11-05 14:12:53 -0600

    Added cl_program_build_info params

--- opencl/f90/OpenCLTypes.f90
+++ opencl/f90/OpenCLTypes.f90
@@ -7,6 +7,8 @@ module OpenCLTypes
   integer, parameter :: cl_bool     = cl_int
   integer, parameter :: cl_bitfield = cl_ulong
 
+  integer, parameter :: cl_program_build_info = cl_uint
+
   !
   ! OpenCL parameters
   !
@@ -35,6 +37,12 @@ module OpenCLTypes
   integer(cl_bitfield), parameter :: CL_MEM_USE_HOST_PTR   = 2**3   ! (1 << 3)
   integer(cl_bitfield), parameter :: CL_MEM_ALLOC_HOST_PTR = 2**4   ! (1 << 4)
   integer(cl_bitfield), parameter :: CL_MEM_COPY_HOST_PTR  = 2**5   ! (1 << 5)
+
+  ! cl_program_build_info
+  !
+  integer(cl_program_build_info), parameter :: CL_PROGRAM_BUILD_STATUS  = 4481  ! 0x1181
+  integer(cl_program_build_info), parameter :: CL_PROGRAM_BUILD_OPTIONS = 4482  ! 0x1182
+  integer(cl_program_build_info), parameter :: CL_PROGRAM_BUILD_LOG     = 4483  ! 0x1183
 
   ! cl_map_flags - bitfield
   !
