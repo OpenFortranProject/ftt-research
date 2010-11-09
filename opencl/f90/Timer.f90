8d6194be9fe12af1f4de981fbbf372bc39b17b38
rasmussn <rasmussn@kirkegaard.lanl.gov>
2010-11-09 09:29:47 -0700

    Converting to processor independent API for time.

--- opencl/f90/Timer.f90
+++ opencl/f90/Timer.f90
@@ -2,27 +2,27 @@ module Timer_mod
 
    use, intrinsic :: ISO_C_BINDING
 
-   type :: MachTimer
+   type :: CPUTimer
       integer :: rank
-      integer(c_int64_t) :: mach_start, mach_end, mach_elapsed
-!      real(c_double) :: mach_start, mach_end, mach_elapsed
+      integer(c_int64_t) :: time_start, time_end, time_elapsed
+!      real(c_double) :: time_start, time_end, time_elapsed
 !   contains
 !      procedure, pass(this) :: init
 !      procedure, pass(this) :: start
 !      procedure, pass(this) :: stop
 !      procedure, pass(this) :: elapsed_time
-   end type MachTimer
+   end type CPUTimer
 
    interface
-      function mach_absolute_time() result(time) bind(C,name="mach_absolute_time")
+      function get_cpu_time() result(time) bind(C,name="get_cpu_time")
          use, intrinsic :: ISO_C_BINDING
          integer(c_int64_t) :: time
       end function
 
-      function print_elapsed_time(mach_elapsed) &
+      function print_elapsed_time(time_elapsed) &
                result(time) bind(C,name="print_elapsed_time")
          use, intrinsic :: ISO_C_BINDING
-         integer(c_int64_t), value :: mach_elapsed
+         integer(c_int64_t), value :: time_elapsed
          real(c_double) :: time
       end function
 
@@ -32,35 +32,35 @@ contains
 
    subroutine init_timer(this)
       implicit none
-!      class(MachTimer) :: this
-      type(MachTimer) :: this
+!      class(CPUTimer) :: this
+      type(CPUTimer) :: this
       this%rank = 0
-      this%mach_start   = mach_absolute_time()
-      this%mach_end     = this%mach_start
-      this%mach_elapsed = 0
+      this%time_start   = get_cpu_time()
+      this%time_end     = this%time_start
+      this%time_elapsed = 0
    end subroutine init_timer
 
    subroutine start(this)
       implicit none
-!      class(MachTimer) :: this
-      type(MachTimer) :: this
-      this%mach_start = mach_absolute_time()
+!      class(CPUTimer) :: this
+      type(CPUTimer) :: this
+      this%time_start = get_cpu_time()
    end subroutine start
 
    subroutine stop(this)
       implicit none
-!      class(MachTimer) :: this
-      type(MachTimer) :: this
-      this%mach_end = mach_absolute_time();
-      this%mach_elapsed = this%mach_elapsed + this%mach_end - this%mach_start
+!      class(CPUTimer) :: this
+      type(CPUTimer) :: this
+      this%time_end = get_cpu_time();
+      this%time_elapsed = this%time_elapsed + this%time_end - this%time_start
    end subroutine stop
 
    subroutine elapsed_time(this)
       implicit none
-!      class(MachTimer) :: this
-      type(MachTimer) :: this
+!      class(CPUTimer) :: this
+      type(CPUTimer) :: this
       real(c_double) :: time
-      time = print_elapsed_time(this%mach_elapsed)
+      time = print_elapsed_time(this%time_elapsed)
    end subroutine elapsed_time
 
 end module Timer_mod
