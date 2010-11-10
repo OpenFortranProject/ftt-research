f548ea5fca5f1bbbf1424e542b8cda0c0c3b7736
rasmussn <rasmussn@kirke.lanl.gov>
2010-11-10 10:46:28 -0700

    cleaned up API so that can get elapsed time without printing it

--- opencl/f90/Timer.f90
+++ opencl/f90/Timer.f90
@@ -16,16 +16,24 @@ module Timer_mod
    interface
       function get_cpu_time() result(time) bind(C,name="get_cpu_time")
          use, intrinsic :: ISO_C_BINDING
+         implicit none
          integer(c_int64_t) :: time
       end function
 
-      function print_elapsed_time(time_elapsed) &
-               result(time) bind(C,name="print_elapsed_time")
+      function print_elapsed_time_c(time_elapsed) &
+               result(time) bind(C,name="print_elapsed_time_c")
          use, intrinsic :: ISO_C_BINDING
+         implicit none
          integer(c_int64_t), value :: time_elapsed
          real(c_double) :: time
       end function
 
+      function cpu_time_to_sec(cpu_elapsed) result(elapsed) bind(C,name="cpu_time_to_sec")
+         use, intrinsic :: ISO_C_BINDING
+         implicit none
+         integer(C_int64_t), value :: cpu_elapsed
+         real(c_double) :: elapsed
+      end function cpu_time_to_sec
    end interface
 
 contains
@@ -55,12 +63,20 @@ contains
       this%time_elapsed = this%time_elapsed + this%time_end - this%time_start
    end subroutine stop
 
-   subroutine elapsed_time(this)
+   subroutine print_elapsed_time(this)
       implicit none
 !      class(CPUTimer) :: this
       type(CPUTimer) :: this
       real(c_double) :: time
-      time = print_elapsed_time(this%time_elapsed)
-   end subroutine elapsed_time
+      time = print_elapsed_time_c(this%time_elapsed)
+   end subroutine print_elapsed_time
+
+   function elapsed_time(this) result(elapsed)
+      implicit none
+!      class(CPUTimer) :: this
+      type(CPUTimer) :: this
+      real(c_double) :: elapsed
+      elapsed = cpu_time_to_sec(this%time_elapsed)
+   end function elapsed_time
 
 end module Timer_mod
