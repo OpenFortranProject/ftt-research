0d5759e9c33ed73f8b0009d3c84f0a6098fb5bcc
rasmussn <rasmussn@kirkelanlgov.local>
2010-11-12 10:47:46 -0700

    Added/implemented shifts in algorithms.  Was using just copies to test.

--- opencl/f90/test_shift_impl.f90
+++ opencl/f90/test_shift_impl.f90
@@ -19,12 +19,23 @@ end subroutine loops_c_f
 
 subroutine shift_f90(A, B, nPad)
    implicit none
-   real, dimension(:,:) :: A, B
+   real, target, dimension(:,:) :: A, B
    integer :: nPad
    integer :: nx, ny, ub(2)
 
+   real, pointer, dimension(:,:) :: iA, lA, rA, uA, dA
+   real, pointer, dimension(:,:) :: iB
+
    ub = ubound(A)
    nx = ub(1); ny = ub(2)
+
+   iB => B(2:nx-1,2:ny-1)
+
+   iA => A(2  :nx-1  ,2  :ny-1  )
+   lA => A(2-1:nx-1-1,2  :ny-1  )
+   rA => A(2+1:nx-1+1,2  :ny-1  )
+   uA => A(2  :nx-1  ,2-1:ny-1-1)
+   dA => A(2  :nx-1  ,2+1:ny-1+1)
 
 !   B(2:nx-1,2:ny-1) = ( A(2:nx-1,2:ny-1)                          &
 !                    +   cshift(A(2:nx-1,2:ny-1), shift=-1, dim=1) &
@@ -32,7 +43,24 @@ subroutine shift_f90(A, B, nPad)
 !                    +   cshift(A(2:nx-1,2:ny-1), shift=-1, dim=2) &
 !                    +   cshift(A(2:nx-1,2:ny-1), shift=+1, dim=2) &
 !                      ) / 5.0
-B(2:nx-1,2:ny-1) = A(2:nx-1,2:ny-1)
+
+!   B(2:nx-1,2:ny-1) = ( A(2  :nx-1  ,2  :ny-1  ) &
+!                    +   A(2-1:nx-1-1,2  :ny-1  ) &
+!                    +   A(2+1:nx-1+1,2  :ny-1  ) &
+!                    +   A(2  :nx-1  ,2-1:ny-1-1) &
+!                    +   A(2  :nx-1  ,2+1:ny-1+1) &
+!                      ) / 5.0
+
+   iB = ( iA + lA + rA + uA + dA ) / 5.0
+
+!print*, "will shift"
+!   B = (A + cshift(A, shift=-1, dim=1))/2.0
+!print*, "did shift"
+
+
+
+
+!   B(2:nx-1,2:ny-1) = A(2:nx-1,2:ny-1)
 end subroutine shift_f90
 
 subroutine loops_f90(A, B, nPad)
@@ -46,13 +74,13 @@ subroutine loops_f90(A, B, nPad)
 
    do j = 2, ny-1
       do i = 2, nx-1
-!         B(i,j) = ( A(i,j)   &
-!                +   A(i-1,j) &
-!                +   A(i+1,j) &
-!                +   A(i,j-1) &
-!                +   A(i,j+1) &
-!                  ) / 5.0
-    B(i,j) = A(i,j)
+         B(i,j) = ( A(i,j)   &
+                +   A(i-1,j) &
+                +   A(i+1,j) &
+                +   A(i,j-1) &
+                +   A(i,j+1) &
+                  ) / 5.0
+         !B(i,j) = A(i,j)
       end do
    end do
 end subroutine loops_f90
