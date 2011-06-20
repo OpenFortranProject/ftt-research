de9221ecde208a7ec60a63268ae1dc7b64ed330c
rasmussn <rasmussn@kirkegaard.lanl.gov>
2011-06-20 14:42:31 -0600

    Initial version

--- papers/local-orientation/nh_core_forall.f90
+++ papers/local-orientation/nh_core_forall.f90
@@ -18,60 +18,63 @@ module nh_core_forall_mod
    implicit none
    private
 
-   public Riem_Solver_forall
+   public Riem_3D_forall
    real, parameter:: dz_max = -0.5               ! (meters)
 
 CONTAINS 
 
-  subroutine Riem_Solver_forall(dt, is, ie, js, je, km, ng, &
-                                akap, ptop, hs,             &
-                                delp, gz)
+  subroutine Riem_3D_forall(ns, bdt, is, ie, js, je, ng, km, cp, gama, cappa, p3, dm3,    &
+                            pm3, w, dz2, pt, quick_p, c_core, ktop, iad)
 !--------------------------------------------
 ! !OUTPUT PARAMETERS
 ! Output: gz: grav*height at edges
 !--------------------------------------------
    implicit none
-   integer, intent(in):: is, ie, js, je, km, ng
-   real, intent(in):: dt         ! the BIG horizontal Lagrangian time step
-   real, intent(in):: akap, ptop
-   real, intent(in):: hs(is-ng:ie+ng,js-ng:je+ng)
-   real, intent(inout), dimension(is-ng:ie+ng,js-ng:je+ng,km):: delp
-   real, intent(out), dimension(is-ng:ie+ng,js-ng:je+ng,km+1):: gz
+  integer, intent(in):: ns, is, ie, js, je, ng,  km
+  integer, intent(in):: iad      ! time step scheme 
+  integer, intent(in):: ktop     ! starting layer for non-hydrostatic dynamics
+                                 ! 1: All non-hydrostatic
+                                 ! 2: top sponge layer is hydrostatic
+  real,    intent(in):: bdt, cp, gama, cappa
+  real,    intent(in), dimension(is:ie,js:je,km):: dm3, pm3
+  logical, intent(in):: quick_p       ! fast algorithm for pressure
+  logical, intent(in):: c_core
+  real, intent(in  ) :: pt (is-ng:ie+ng,js-ng:je+ng,km)
+! IN/OUT:
+  real, intent(inout):: dz2(is:ie,km)
+  real, intent(inout)::   w(is-ng:ie+ng,js-ng:je+ng,km)
+  real, intent(out  )::  p3(is-ng:ie+ng,js-ng:je+ng,km+1)
 
-! Local:
-   real, dimension(is:ie,km):: dm
-   real    :: gama, rgrav, ptk
+! --- Local 1D copies -----
+  real, dimension(km):: c2, p2, pt2
+  real, dimension(km):: r_p, r_n, rden, dz, dm, wm, dts, pdt
+  real, dimension(km+1):: m_bot, m_top, r_bot, r_top, time_left, pe1, pbar, wbar
+
+! Local scalars:
    integer :: i, j, k
 
 !
 ! The first problem to solve is what is the dimension and size of
 ! the forall loop.  This is important because it relates to the
 ! number of threads running the problem.  It looks like this is
-! fundamentally a 3D problem.
-!
+! fundamentally a 3D problem.  The way the loops are structured
+! it looks like we have to host the j loop out of the procedure
+! kernel and let the forall loop over i and k.  It would be
+! better to do the forall over i and j (contiguous memory) but
+! that is not the way the code is structured.
 
-   forall (j=js:je, k=1:km, i=is:ie)
+   do concurrent (i=is:ie, j=js:je)
+      block
+         real, dimension(km) :: dm, wm
 
-!
-! Scalar assignments, not a problem as these scalars
-! will have local storage and will be calculated for each thread.
-! So logically they are within the loop which leads to compiler
-! warnings.
-!
-       gama = 1./(1.-akap)
-      rgrav = 1./grav
-        ptk = ptop ** akap
+         do k=ktop,km
+            dm(k) = dm3(i,j,k)
+            wm(k) = w(i,j,k)*dm(k)
+         enddo
 
-!
-! However, this is a problem as the arrays aren't the same size.
-! This means we have to rethink the problem.  We could treat
-! this as a 2D problem and call the kernel procedure for different
-! j values.
-!
-      dm(i,k) = delp(i,j,k)
+      end block
+   end do concurrent
 
-   end forall
-
-end subroutine Riem_Solver_forall
+end subroutine Riem_3D_forall
 
 end module nh_core_forall_mod
