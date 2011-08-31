630193778d114700665111856a98783a5d8b152c
rasmussn <rasmussn@kirkegaard.lanl.gov>
2011-08-31 14:55:52 -0600

    Removed NON_ELEMENTAL by using a derived type.

--- papers/local-orientation/code/convolve/convolve_elemental.f90
+++ papers/local-orientation/code/convolve/convolve_elemental.f90
@@ -1,9 +1,21 @@
-   pure elemental subroutine convolve(SmoothedImage, Image, F, N)
-      real, intent(out) :: SmoothedImage
-      real, intent(in), EXTEND(0) :: Image(-N:N,-N:N)
-      real, intent(in), NON_ELEMENTAL :: F(-N:N,-N:N)
-      integer, intent(in) :: N
+module Filter
 
-      SmoothedImage = sum(F*Image)
+   integer, parameter :: N
+
+   type :: FilterPatch
+      real :: p(-N:N,-N:N)
+   end type FilterPatch
+
+contains
+
+   pure elemental subroutine convolve(SmoothedImage, Image, Filter)
+      real, intent(out) :: SmoothedImage
+      real, intent(in), EXTEND :: Image(-N:N,-N:N)
+      type(FilterPatch), intent(in) :: Filter
+
+      SmoothedImage = sum(Filter%p * Image)
 
    end subroutine convolve
+
+
+end module Filter
