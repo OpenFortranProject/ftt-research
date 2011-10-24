48d51dbbbb449669df612fb426a8a97a764d9b39
Jason Dagit <dagit@galois.com>
2011-10-24 15:32:21 -0700

    add implicitSave checker

---
+++ checkers/implicitSave/implicitSaveMain.C
@@ -0,0 +1,10 @@
+//
+// Do not modify this file
+//
+#include "rose.h"
+#include "compass.h"
+extern const Compass::Checker* const implicitSaveChecker;
+extern const Compass::Checker* const myChecker = implicitSaveChecker;
+
+#include "compass.C"
+#include "compassTestMain.C"
