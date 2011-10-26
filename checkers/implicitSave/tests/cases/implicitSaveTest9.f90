b92698c16cda7630b1423c5f645ba449b821d38f
Jason Dagit <dagit@galois.com>
2011-10-26 15:48:58 -0700

    implicitSave: add test cases that test transitive checking

---
+++ checkers/implicitSave/tests/cases/implicitSaveTest9.f90
@@ -0,0 +1,22 @@
+subroutine f
+  integer :: x = 4
+  call h(x)
+  print *,x
+end subroutine f
+
+subroutine h(x)
+  integer, intent(out) :: x
+  call g(x)
+end subroutine h
+
+subroutine g(x)
+  integer, intent(inout) :: x
+
+  x = x + 1
+end subroutine g
+
+program main
+  call f
+  call f
+  call f
+end program main
