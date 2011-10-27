2a1ccaffbe546aeae0c05c4ae91ba1b6a7aa0ee5
rasmussn <rasmussn@kirke.lanl.gov>
2011-10-27 11:10:39 -0600

    Changed width for Lena image.

---
+++ papers/LOPe/code/convolve/filter_params.F90
@@ -0,0 +1,24 @@
+#include "npad.h"
+
+module FilterParams
+   use ISO_C_BINDING
+
+   ! WARNING, must also change in convolve.cl
+   integer(c_int), parameter :: NPAD = PADDING
+
+   integer, parameter :: G_WIDTH  = 480
+   integer, parameter :: G_HEIGHT = 528
+
+   integer(c_int), parameter :: NXP = 1 + 2*NPAD
+   integer(c_int), parameter :: NYP = NXP
+
+   integer(c_size_t), parameter :: NX   = G_WIDTH
+   integer(c_size_t), parameter :: NY   = G_HEIGHT
+   integer(c_size_t), parameter :: NXEX = NX + 2*NPAD
+   integer(c_size_t), parameter :: NYEX = NY + 2*NPAD
+   integer(c_size_t), parameter :: NXL  = 16
+   integer(c_size_t), parameter :: NYL  = 16
+
+   integer(c_size_t), parameter :: SIZE_ELEMENT = 4
+
+end module FilterParams
