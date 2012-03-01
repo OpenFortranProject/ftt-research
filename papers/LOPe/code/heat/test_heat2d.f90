7f224d89297b1956ab7bf3750de8932db3dc319d
rasmussn <rasmussn@kirkegaard.lanl.gov>
2012-03-01 11:17:10 -0700

    Changed to use more of LOPe semantics.

--- papers/LOPe/code/heat/test_heat2d.f90
+++ papers/LOPe/code/heat/test_heat2d.f90
@@ -23,9 +23,9 @@ module heat_transfer
    real,    parameter :: MAX_TEMP    = 100.0
 
    interface
-      pure elemental function adv_time(T)
-         real, intent(in) :: T
-      end function adv_time
+      CONCURRENT subroutine adv_time_lope(T)
+         real, intent(in out) :: T(:,:)
+      end subroutine adv_time_lope
    end interface
 
 end module heat_transfer
@@ -36,28 +36,28 @@ program test_heat2d
    implicit none
    integer :: i
 
-   real, dimension(0:NX-1,0:NY-1) :: T1, T2
+   real, allocatable, dimension(:,:) :: T
   
    real, parameter :: alpha = .001
    real, parameter :: dx = 1.0 / WIDTH
    real, parameter :: dy = 1.0 / HEIGHT
    real, parameter :: dt = .01 * (alpha / 4.0) * ((1.0 / (dx*dx)) + (1.0 / (dy*dy)))
 
+   ! allocate memory
+   !
+   allocate(T(HEIGHT,WIDTH), HALO=(1,1))
+
    ! initialize cell temperature
    !
-   T1 = MAX_TEMP
-   T2 = 0.0
+   T = MAX_TEMP
 
-   T1(:,0)    = 0.0
-   T1(:,NY-1) = 0.0
-   T1(0,:)    = 0.0
-   T1(NX-1,:) = 0.0
+   ! need to figure out syntax for setting halo regions
+   !
 
    ! time loop
    !
-   do i = 1, NTIME_STEPS/2
-      T2(1:WIDTH,1:HEIGHT) = adv_time(T1(1:WIDTH,1:HEIGHT))
-      T1(1:WIDTH,1:HEIGHT) = adv_time(T2(1:WIDTH,1:HEIGHT))
+   do i = 1, NTIME_STEPS
+      call adv_time_lope(T)
    end do
 
 end program test_heat2d
