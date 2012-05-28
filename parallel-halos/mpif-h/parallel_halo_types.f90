061bee3a093d7c6475cd933424bfe071df366a81
Craig Rasmussen <rasmus@CAS-IT0064M.local>
2012-05-27 21:55:13 -0700

    Initial version.

---
+++ parallel-halos/mpif-h/parallel_halo_types.f90
@@ -0,0 +1,43 @@
+Module Parallel_Halo_Types
+!=====================================================================
+! Module Parallel_Halo_Types defines types (C structs) for halo usage
+! in a parallel context.
+!
+!=====================================================================
+
+Use, Intrinsic :: ISO_C_BINDING
+Use            :: MPI
+Implicit None
+Save
+ 
+Type, Bind(C) :: Context
+   !... Parallel context
+   !--------------------
+   Integer(C_INT) :: rank          ! id of this process
+   Integer(C_INT) :: size          ! number of participating processes
+   Integer        :: comm          ! communicator for this context
+End Type Context
+
+Type, Bind(C) :: Halo
+   !... Halo size
+   !-------------
+   Integer(C_INT) :: hlx, hrx          ! halo size (x-direction)
+   Integer(C_INT) :: hly, hry          ! halo size (y-direction)
+   Integer(C_INT) :: hlz, hrz          ! halo size (z-direction)
+End Type Halo
+
+Type, Bind(C) :: Topology
+   !... Cartesian topology
+   !----------------------
+   Integer(C_INT) :: Left, Right       ! Neighbor ranks (x-direction)
+   Integer(C_INT) :: Bottom, Top       ! Neighbor ranks (y-direction)
+   Integer(C_INT) :: Front, Back       ! Neighbor ranks (z-direction)
+
+   !... Cartesian decomposition
+   !---------------------------
+   Integer(C_INT) :: npex=0            ! Number of processors X direction
+   Integer(C_INT) :: npey=0            ! Number of processors Y direction
+   Integer(C_INT) :: npez=0            ! Number of processors Z direction
+End Type Topology
+
+End Module Parallel_Halo_Types
