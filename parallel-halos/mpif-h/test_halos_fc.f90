85354668c6fbaf424d3f715e09b8cc308bf1bd63
Craig Rasmussen <rasmus@CAS-IT0064M.local>
2012-05-28 14:40:34 -0700

    Use ../ version

--- parallel-halos/mpif-h/test_halos_fc.f90
+++
@@ -1,9 +0,0 @@
-program test_halos_fc
-  interface
-     subroutine test_halos_c() bind(C, name="test_halos_c")
-     end subroutine test_halos_c
-  end interface
-
-  call test_halos_c
-
-end program test_halos_fc
