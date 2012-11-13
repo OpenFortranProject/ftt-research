bd0cedaea0e4ea8e45d523faaae0d9c75aaa3168
Jason Dagit <dagit@galois.com>
2012-11-12 17:11:20 -0800

    aterm-utils: Rewrite matching to use functions and MonadPlus

--- checkers/haskell/aterm-utils/src/ATerm/Matching.hs
+++ checkers/haskell/aterm-utils/src/ATerm/Matching.hs
@@ -1,46 +1,44 @@
 {-# LANGUAGE PatternGuards #-}
--- TODO: haddock
 module ATerm.Matching
 ( -- * Types
-  ATermMatcher(..)
-, Match(..)
-, Binding(..)
--- * Binding and matching
-, matches
-, bindMatches
+  Binding(..)
 -- * Combinators
-, exactlyS     -- String -> ATermMatcher
-, exactlyI     -- Integer -> ATermMatcher
-, exactlyL     -- [ATermMatcher] -> ATermMatcher
-, exactlyA     -- String -> Match ATermMatcher] -> ATermMatcher
-, contains     -- [ATermMatcher] -> Match [ATermMatcher]
-, containsL    -- [ATermMatcher] -> ATermMatcher
-, containsA    -- Match String -> [ATermMatcher] -> ATermMatcher
-, bindT        -- ATermMatcher
-, bindL        -- ATermMatcher
-, bindI        -- ATermMatcher
-, bindS        -- ATermMatcher
-, bindA        -- ATermMatcher
+, exactlyS
+, exactlyI
+, exactlyL
+, exactlyA
+, exactlyNamed
+, contains
+, containsL
+, containsA
+, containsChildren
+, bindT
+, bindL
+, bindI
+, bindS
+, bindA
 ) where
 
 import ATerm.AbstractSyntax
-import Data.Maybe ( isJust, catMaybes )
-import Control.Applicative
+import qualified ATerm.Utilities as U
+import Control.Monad -- For MonadPlus
+import Data.Maybe ( catMaybes )
 
--- | While these constructors are exported, the recommend use is by
--- the combinators not direct use.
-data ATermMatcher = AMAppl  (Match String)  (Match [ATermMatcher])
-                  | AMList                  (Match [ATermMatcher])
-                  | AMInt   (Match Integer)
-                  | AMATerm -- ^ can only be a wildcard
+-- | Binding type gives you back the parts of the ATerm that
+-- that match. There is also a case for when no bindings are
+-- generated.
+data Binding = NoBinding         -- ^ This mempty for this type
+             | BoundTerm Int     -- ^ The index into the ATermTable of the matching ATerm
+             | BoundList [Int]   -- ^ The list of indexes into the ATermTable that match
+             | BoundInt  Integer -- ^ The matching Integer
+             | BoundStr  String  -- ^ The matching String
+             | BoundAppl Int     -- ^ The index into the ATermTable for the ShAAppl term
   deriving (Eq, Read, Show, Ord)
 
--- | While these constructors are exported, the recommend use is by
--- the combinators not direct use.
-data Match a = Exactly a  -- ^ Matching via Eq a, no binding generated on match
-             | Any        -- ^ Wildcard, a binding will be created on match
-             | Contains a -- ^ Meant for subset matches, use the combinators
-  deriving (Eq, Read, Show, Ord)
+-- | Turns anything with an Eq instance into a
+-- matcher by using (==). No binding is generated.
+exactly :: (MonadPlus m, Eq a) => a -> a -> m Binding
+exactly a b = guard (a == b) >> return NoBinding
 
 -- * ATermMatcher Combinators
 
@@ -49,147 +47,107 @@ data Match a = Exactly a  -- ^ Matching via Eq a, no binding generated on match
 ---------------------------------------------------------------------
 
 -- | Matches exactly the string 's' within the ATerm
-exactlyS :: String -> ATermMatcher
-exactlyS s = AMAppl (Exactly s) (Exactly [])
+exactlyS :: MonadPlus m => String -> ATermTable -> m Binding
+exactlyS s t = case getATerm t of
+  ShAAppl s' [] _ -> exactly s s'
+  _               -> mzero
 
 -- | Matches exactly the integer 'i' within the ATerm
-exactlyI :: Integer -> ATermMatcher
-exactlyI i = AMInt (Exactly i)
+exactlyI :: MonadPlus m => Integer -> ATermTable -> m Binding
+exactlyI i t = case getATerm t of
+  ShAInt i' _ -> exactly i i'
+  _           -> mzero
 
 -- | Matches exactly the list 'xs' within the ATerm
-exactlyL :: [ATermMatcher] -> ATermMatcher
-exactlyL xs = AMList (Exactly xs)
+exactlyL :: MonadPlus m => [ATermTable -> m a] -> ATermTable -> m [a]
+exactlyL ms t = case getATerm t of
+  ShAList ls _ -> do
+    _ <- guard (length ms == length ls)
+    sequence (zipWith (\m i -> m (getATermByIndex1 i t)) ms ls)
+  _ -> mzero
 
 -- | Matches the string exactly
-exactlyA :: String -> Match [ATermMatcher] -> ATermMatcher
-exactlyA s ms = AMAppl (Exactly s) ms
+exactlyA :: MonadPlus m => String -> [ATermTable -> m a] -> ATermTable -> m [a]
+exactlyA s ms t = case getATerm t of
+  ShAAppl s' ls _ -> do
+    _ <- exactly s s'
+    _ <- guard (length ms == length ls)
+    sequence (zipWith (\m i -> U.app m t i) ms ls)
+  _ -> mzero
+
+-- | Looks for an Appl with name 's' and any children
+exactlyNamed :: MonadPlus m => String -> ATermTable -> m Binding
+exactlyNamed s t = case getATerm t of
+  ShAAppl s' _ _ -> do
+    _ <- exactly s s'
+    return NoBinding
+  _ -> mzero
 
 ---------------------------------------------------------------------
 -- ** Partial matchers, ie., they just specify part of the structure
 ---------------------------------------------------------------------
 
+containsChildren :: [ATermTable -> Maybe a] -> [Int] -> ATermTable -> Maybe [a]
+containsChildren ms is t = return $
+  catMaybes [ m (getATermByIndex1 i t) | i <- is, m <- ms ]
+
 -- | Matches a partial specification against the children. The
 -- matching is maximised so that if the pattern occurs more than
 -- once it is matched each time it appears.
-contains :: [ATermMatcher] -> Match [ATermMatcher]
-contains = Contains
+contains :: [ATermTable -> Maybe a] -> ATermTable -> Maybe [a]
+contains ms t = case getATerm t of
+  ShAAppl _ ls _ -> containsChildren ms ls t
+  ShAList   ls _ -> containsChildren ms ls t
+  _              -> mzero
 
 -- | Matches a partial specification of a sub aterm List. The
 -- matching is maximised so that if the pattern occurs more than
 -- once it is matched each time it appears.
-containsL :: [ATermMatcher] -> ATermMatcher
-containsL = AMList . Contains
+containsL :: [ATermTable -> Maybe a] -> ATermTable -> Maybe [a]
+containsL ms t = case getATerm t of
+  ShAList ls _ -> containsChildren ms ls t
+  _            -> mzero
 
 -- | Matches a partial specification of an Appl. The matching is
 -- maximised so that if the pattern occurs more than once it is match
 -- each time it appears.
-containsA :: Match String -> [ATermMatcher] -> ATermMatcher
-containsA m ams = AMAppl m (Contains ams)
+containsA :: String -> [ATermTable -> Maybe a] -> ATermTable -> Maybe [a]
+containsA s ams t = case getATerm t of
+  ShAAppl s' ls _ -> do
+    _ <- exactly s s'
+    containsChildren ams ls t
+  _               -> mzero
 
 ---------------------------------------------------------------------
 -- ** Wildcard matchers, these create bindings when they match
 ---------------------------------------------------------------------
 
 -- | Matches any ATerm and generates a binding to that term
-bindT :: ATermMatcher
-bindT = AMATerm
-
+bindT :: MonadPlus m => ATermTable -> m Binding
+bindT t = return (BoundTerm (getTopIndex t))
+  
 -- | Matches any list and generates a binding to that list
-bindL :: ATermMatcher
-bindL = AMList Any
+bindL :: MonadPlus m => ATermTable -> m Binding
+bindL t = case getATerm t of
+  ShAList ls _ -> return (BoundList ls)
+  _            -> mzero
 
 -- | Matches any integer and generates a binding to that integer
-bindI :: ATermMatcher
-bindI = AMInt Any
+bindI :: MonadPlus m => ATermTable -> m Binding
+bindI t = case getATerm t of
+  ShAInt i _ -> return (BoundInt i)
+  _          -> mzero
 
 -- | Matches any string and generates a binding to that string
 -- Strings have the form (Appl somestring [] []).
 -- Not to be confused with matching the the string part of an Appl.
-bindS :: ATermMatcher
-bindS = AMAppl Any (Exactly [])
+bindS :: MonadPlus m => ATermTable -> m Binding
+bindS t = case getATerm t of
+  ShAAppl s [] [] -> return (BoundStr s)
+  _               -> mzero
 
 -- | Matches any Appl and generates a binding to that Appl
-bindA :: ATermMatcher
-bindA = AMAppl Any Any
-
--- TODO: Make some convenience functions for working with Bindings
-
-data Binding = BoundTerm Int     -- ^ The index into the ATermTable of the matching ATerm
-             | BoundList [Int]   -- ^ The list of indexes into the ATermTable that match
-             | BoundInt  Integer -- ^ The matching Integer
-             | BoundStr  String  -- ^ The matching String
-             | BoundAppl Int     -- ^ The index into the ATermTable for the ShAAppl term
-  deriving (Eq, Read, Show, Ord)
-
--- | Tests for a match but discards any bindings
-matches :: ATermMatcher -> ATermTable -> Bool
-matches m at = isJust (bindMatches m at)
-
--- The obvious question: Is there a sane way to do this with fewer
--- cases? It's a bit of combinatorial explosion at the moment.
--- | Tests for a match and binds the wildcards in the matcher against
--- the respective terms of the match.
-bindMatches :: ATermMatcher -> ATermTable -> Maybe [Binding]
-bindMatches = go
-  where
-  matchChildren ms is t =
-    concat <$> sequence (map (\(m,i) -> bindMatches m (getATermByIndex1 i t))
-                             (zip ms is))
-  containsChildren ms is t =
-    concat (catMaybes [ bindMatches m (getATermByIndex1 i t) | i <- is, m <- ms ])
-  -- AMATerm cases:
-  go AMATerm t = Just [BoundTerm (getTopIndex t)]
-  -- AMInt cases:
-  go (AMInt Any) t
-    | ShAInt i _ <- getATerm t = Just [BoundInt i]
-  go (AMInt (Exactly i)) t
-    | ShAInt i' _ <- getATerm t
-    , i == i' = Just [] -- we match, but no wild cards means no binders
-  go (AMInt (Contains i)) t -- this doesn't really make much sense, but we allow it
-    | ShAInt i' _ <- getATerm t
-    , i == i' = Just [] -- they have to be ints so no wild cards possible
-  -- AMList cases:
-  go (AMList Any) t
-    | ShAList l _ <- getATerm t = Just [BoundList l]
-  go (AMList (Exactly ms)) t
-    | ShAList is _ <- getATerm t
-    , length ms == length is = matchChildren ms is t
-  go (AMList (Contains ls)) t
-    | ShAList is _ <- getATerm t = Just $ containsChildren ls is t
-  -- AMAppl cases:
-  go (AMAppl Any Any) t
-    | ShAAppl _ _ _ <- getATerm t = Just [BoundAppl (getTopIndex t)]
-  go (AMAppl (Exactly s) Any) t
-    | ShAAppl s' ls _ <- getATerm t
-    , s == s' = Just [BoundList ls]
-  go (AMAppl Any (Exactly ms)) t
-    | ShAAppl _ is _ <- getATerm t
-    , length ms == length is = (BoundAppl (getTopIndex t) :) <$> matchChildren ms is t
-  -- Matching strings requires this special case
-  go (AMAppl (Exactly s) (Exactly [])) t
-    | ShAAppl s' [] _ <- getATerm t
-    , s == s' = Just [BoundStr s]
-  go (AMAppl (Exactly s) (Exactly ms)) t
-    | ShAAppl s' is _ <- getATerm t
-    , s == s'
-    , length ms == length is = matchChildren ms is t
-  go (AMAppl (Contains s) Any) t -- this doesn't really make much sense, but we allow it
-    | ShAAppl s' _ _ <- getATerm t
-    , s == s' = Just [] -- they have to be strings so no wild cards possible
-  go (AMAppl (Contains s) (Exactly [])) t
-    | ShAAppl s' [] _ <- getATerm t
-    , s == s' = Just [] -- they have to be strings so no wild cards possible
-  go (AMAppl (Contains s) (Exactly ms)) t
-    | ShAAppl s' is _ <- getATerm t
-    , s == s'
-    , length ms == length is = matchChildren ms is t
-  go (AMAppl Any (Contains ls)) t
-    | ShAAppl _ is _ <- getATerm t = Just $ (BoundAppl (getTopIndex t)) : containsChildren ls is t
-  go (AMAppl (Exactly s) (Contains ls)) t
-    | ShAAppl s' is _ <- getATerm t
-    , s' == s = Just $ containsChildren ls is t
-  go (AMAppl (Contains s) (Contains ls)) t -- this doesn't really make sense, but we allow it
-    | ShAAppl s' is _ <- getATerm t
-    , s == s' = Just $ containsChildren ls is t
-  -- catch all failure case
-  go _ _ = Nothing -- Welp, no takers
+bindA :: MonadPlus m => ATermTable -> m Binding
+bindA t = case getATerm t of
+  ShAAppl _ _ _ -> return (BoundAppl (getTopIndex t))
+  _             -> mzero
