49e9bf47f948535396c33b7e70a05bc1a449f960
David Lazar <lazar6@illinois.edu>
2013-02-04 12:26:35 -0600

    Derive Data and Typeable for mini-Fortran's syntax.

--- mini-fortran/src/Language/MiniFortran.hs
+++ mini-fortran/src/Language/MiniFortran.hs
@@ -3,6 +3,7 @@ module Language.MiniFortran where
 
 import Data.Map ( Map )
 import qualified Data.Map as M
+import Data.Data (Data)
 import Data.Typeable
 
 type Variable = String
@@ -16,7 +17,7 @@ data NumericExpr
   | NumericExpr :/: NumericExpr
   | NumericMinus NumericExpr -- ^ unary minus
   | NumericVal   ValueExpr
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 infixl 6 :+:, :-:
 infixl 7 :*:, :/:
@@ -34,7 +35,7 @@ data LogicExpr
   | NumericExpr :<=: NumericExpr
   | NumericExpr :==: NumericExpr
   | NumericExpr :/=: NumericExpr
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 infixr 3 :&&:, :^:
 infixr 2 :||:
@@ -45,24 +46,24 @@ data Expr
   = NumE NumericExpr
   -- | Logical & Comparison, not to be confused with log base e
   | LogE LogicExpr
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data ValueExpr
   = RefExpr Ref
   | Lit    Literal
   | Slice  SliceExpr
   | Func   Variable [Expr]
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data Ref
   = VarRef   Variable
   | ArrayRef Variable [IndexExpr] -- ^ array name and list of indices
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data IndexExpr
   = IdxExpr  NumericExpr
   | IdxSlice SliceExpr
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data SliceExpr
   = SliceExpr
@@ -70,17 +71,17 @@ data SliceExpr
   , seUpper  :: UpperBound
   , seStride :: NumericExpr -- ^ defaults to 1
   }
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data UpperBound
   = UBExpr NumericExpr
   | UpperBound
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data LowerBound
   = LBExpr NumericExpr
   | LowerBound
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 -- | a group of statements, not a basic block
 type Block = [Stmt]
@@ -99,7 +100,7 @@ data LoopBounds
   , lbTo   :: NumericExpr
   , lbStep :: NumericExpr -- ^ Default is 1
   }
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data Stmt
   = Write String -- ^ What else...
@@ -112,54 +113,54 @@ data Stmt
   | DoWhile LogicExpr          Block -- ^ loops with a conditional expr
   | Call Variable [Expr] -- ^ Procedure/Function calls
   | Ref :=: Expr
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data NumericType
   = TyInt
   | TyFloat
   | TyDouble
   | TyComplex
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data LogicalType
   = TyBool
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data LiteralType
   = TyNumeric NumericType
   | TyLogical LogicalType
   | TyString
   | TyChar
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data ArrayType
   = TyArray Dimension LiteralType
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data DataType
   = TyArr  ArrayType
   | TyLit  LiteralType
   | TyUser UserDefinedType
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data UserDefinedType = UserDefinedType Variable [(Variable, DataType)]
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data ProcType = ProcType [DataType]
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data FuncType = FuncType [DataType] DataType
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data Type
   = TyData DataType
   | TyProc ProcType
   | TyFunc FuncType
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data Dimension
   = Dim [(LowerBound,UpperBound)]
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data Decl a
   = Decl
@@ -167,7 +168,7 @@ data Decl a
   , declType       :: a
   , declAttributes :: [Attribute]
   }
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 type VarDecl  = Decl DataType
 type ProcDecl = Decl ProcType
@@ -179,7 +180,7 @@ data Def a
   , defVarDecls :: [VarDecl]
   , defBody     :: Block
   }
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 type ProcDef = Def ProcDecl
 type FuncDef = Def FuncDecl
@@ -190,18 +191,18 @@ data Attribute
   -- LoPE annotations:
   | Halo [Extent] BoundaryCond
   | Intent
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data Extent = Extent -- TODO: what should this be?
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data BoundaryCond = BoundaryCond -- TODO: what should this be?
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 data Intent
   = In
   | Out
   | InOut
-  deriving (Read, Show, Eq, Ord, Typeable)
+  deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 type SymbolTable = Map Variable (Decl Type)
