fcae45ad82ea0e6bf567adc6a58d963b163b49cb
Craig Rasmussen <rasmus@uoregon.edu>
2015-01-16 16:52:06 -0800

    Added additional output and error checking.

--- parallel-halos/test_halos.f90
+++ parallel-halos/test_halos.f90
@@ -1,18 +1,36 @@
 Program test_halo
 !=====================================================================
 
-Use  ::  Parallel_Halo
+Use  ::  Parallel_Halo , only : Context, Parallel_Topology, Parallel_Start, Parallel_End
+Use  ::  Parallel      , only : Left, Right, Bottom, Top, Front, Back
+Use  ::  MPI_F08
 Implicit None
 
+!... Local Variables
+!-------------------
 Type(Context) :: aContext
+Integer       :: ndims, dims(3)
 
 !---------------------------------------------------------------------
 
 Call Parallel_Start(aContext)
 
-print *, aContext%rank, aContext%size
+ndims = 2
+dims  = [aContext%size/2,0,0]
 
-Call Parallel_Topology(aContext)
+Call Parallel_Topology(aContext, ndims, dims)
+
+if (Front /= aContext%rank) STOP 'ERROR: Front not me'
+if (Back  /= aContext%rank) STOP 'ERROR: Back  not me'
+
+if (aContext%rank == 0) then
+   print *, "(size,dims) =", aContext%size, dims
+   print *
+end if
+
+call MPI_Barrier(MPI_COMM_WORLD)
+
+print "(1I6,':  ',4I6)", aContext%rank, Left, Right, Bottom, Top
 
 Call Parallel_End(aContext)
 
