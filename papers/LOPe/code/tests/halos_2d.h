f67668252777b45d3d806487830f2e9ae9df9c7f
Craig Rasmussen <rasmus@uoregon.edu>
2015-01-20 11:00:56 -0800

    Added HALO2 macro and cleaned up comments.

--- papers/LOPe/code/tests/halos_2d.h
+++ papers/LOPe/code/tests/halos_2d.h
@@ -1,9 +1,8 @@
 #define K1_ (get_global_id(0))
 #define K2_ (get_global_id(1))
 
-//#define N1_ (16*1024*1024)
-#define N1_ (16)
-#define N2_ (4)
+#define N1_ (64*1024)
+#define N2_ (1024)
 
 #define A_H1L_ 1
 #define A_H1R_ 1
@@ -27,12 +26,15 @@
 #define C_S1_  1
 #define C_S2_  (C_H1L_ + C_H1R_ + N1_)
 
-// 1D halo size for the given SIDE of the array NAME
+// First dimension (Left/Right) halo size for the given SIDE of the array NAME
 #define HALO1(NAME,SIDE)   ( (NAME ## _H1 ## SIDE ## _) )
+
+// Second dimension (Bottom/Top) halo size for the given SIDE of the array NAME
+#define HALO2(NAME,SIDE)   ( (NAME ## _H2 ## SIDE ## _) )
 
 // 1D index for the array NAME and offset OFF
 #define IDX1(NAME,OFF)     ( (OFF) + (K1_) + (NAME ## _H1L_) )
 
-// When 2D implemented will need stride for second dimension
+// 2D index for the array NAME and offset OFF
 #define IDX2(NAME,OFF1,OFF2) (  ((OFF1) + (K1_) + (NAME ## _H1L_)) * (NAME ## _S1_) \
                               + ((OFF2) + (K2_) + (NAME ## _H2L_)) * (NAME ## _S2_)  )
