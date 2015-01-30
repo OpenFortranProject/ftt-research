12251b9d4249f92e47f2f8564018ec1ebb9dc4f0
rasmussn <rasmus@uoregon.edu>
2015-01-30 09:27:20 -0800

    Added Halo_Exchange3D

--- parallel-halos/parallel_mpif08.f90
+++ parallel-halos/parallel_mpif08.f90
@@ -667,6 +667,131 @@ Array(sx:ex,sy:ey,ez+1) = RESHAPE(source=zrecv(:),shape=[mx,my])
  
 End Subroutine Exchange3D
 
+Subroutine Halo_Exchange3D (sendbuf, recvbuf, nx,hx, ny,hy, nz,hz)
+!=====================================================================
+!
+!        Halo_Exchange3D
+!
+! Subroutine Halo_Exchange3D controls the exchange of information between
+! processors for an array with halo data sendbuf and recvbuf.  Each process
+! sends data to its neighbors (Right/Left/Bottom/Top/Back/Front) and receives
+! data from its neighbors (Left/Right/Top/Bottom/Front/Back).
+!
+! Author :
+!        Craig Rasmussen, University of Oregon
+!
+! History :
+!        2015-01-16, original version
+!
+! Inputs :
+!        sendbuf   Buffer containing outgoing (to neighbors) halo-cell information
+!        nx,ny,nz  Array size in x,y
+!        hx,hy,hz  halo width in x,y
+!
+! Outputs :
+!        recvbuf  Buffer for incoming (from neighbors) halo-cell information
+!
+! Externals :
+!        MPI_SENDRECV
+!
+! References :
+!        "Using MPI, Portable Parallel Programming with the Message-
+!         Passing Interface", by W. Gropp, E. Lusk, A. Skejellum.
+!         (based on the code exchng2 listed in twod.f)
+!
+! Notes :
+!        This procedure assumes that the halos for the arrays have
+!        already been combined into a single array.  The order
+!        in the halo buffer is (Left,Right,Bottom,Top) and the halo
+!        widths of each halo region can be greater than 1.  Using
+!        a precombined halo buffer for an array can be more efficient
+!        in the case of multiple memory levels where only the halo
+!        buffers need to be exchanged.  In this instance the corresponding
+!        array may not even be available.
+!
+!        When SENDRECV is used, data flows simultaneously in both
+!        directions (logically, at least) and cycles in the MPI
+!        communication pattern DO NOT lead to deadlock.
+!
+!=====================================================================
+Use MPI_F08
+Implicit None
+
+Integer, Intent(In) :: nx,hx, ny,hy, nz,hz
+Real, Dimension(*), Intent(In ) :: sendbuf  ! size is (2*ny*hx+2*nx*hy+2*nz*nh)
+Real, Dimension(*), Intent(Out) :: recvbuf  ! size is (2*ny*hx+2*nx*hy+2*nz*nh)
+
+!... Local Variables
+!-------------------
+Type(MPI_Status) :: status
+
+Integer :: sendoff, recvoff
+Integer :: ierror
+Integer :: n
+!---------------------------------------------------------------------
+
+!... X-direction
+!---------------
+sendoff = 0
+recvoff = sendoff + ny*nz*hx
+
+n = ny*nz * hx
+
+Call MPI_Sendrecv (sendbuf(1+sendoff), n, MPI_REAL,  Left, 1, &
+                 & recvbuf(1+recvoff), n, MPI_REAL, Right, 1, &
+                 & MPI_COMM_CART, status, ierror)
+
+!... X+direction
+!---------------
+sendoff = sendoff + n
+recvoff = recvoff - n
+
+Call MPI_Sendrecv (sendbuf(1+sendoff), n, MPI_REAL,  Right, 2, &
+                 & recvbuf(1+recvoff), n, MPI_REAL,   Left, 2, &
+                 & MPI_COMM_CART, status, ierror)
+
+!... Y-direction
+!---------------
+sendoff = sendoff + n
+recvoff = sendoff + nx*nz*hy
+
+n = nx*nz * hy
+
+Call MPI_Sendrecv (sendbuf(1+sendoff), n, MPI_REAL, Bottom, 3, &
+                 & recvbuf(1+recvoff), n, MPI_REAL,    Top, 3, &
+                 & MPI_COMM_CART, status, ierror)
+
+!... Y+direction
+!---------------
+sendoff = sendoff + n
+recvoff = recvoff - n
+
+Call MPI_Sendrecv (sendbuf(1+sendoff), n, MPI_REAL,    Top, 4, &
+                 & recvbuf(1+recvoff), n, MPI_REAL, Bottom, 4, &
+                 & MPI_COMM_CART, status, ierror)
+
+!... Z-direction
+!---------------
+sendoff = sendoff + n
+recvoff = sendoff + nx*ny*hz
+
+n = nx*ny * hz
+
+Call MPI_Sendrecv (sendbuf(1+sendoff), n, MPI_REAL, Front, 5, &
+                 & recvbuf(1+recvoff), n, MPI_REAL,  Back, 5, &
+                 & MPI_COMM_CART, status, ierror)
+
+!... Z+direction
+!---------------
+sendoff = sendoff + n
+recvoff = recvoff - n
+
+Call MPI_Sendrecv (sendbuf(1+sendoff), n, MPI_REAL,  Back, 6, &
+                 & recvbuf(1+recvoff), n, MPI_REAL, Front, 6, &
+                 & MPI_COMM_CART, status, ierror)
+
+End Subroutine Halo_Exchange3D
+
 Subroutine Halo_Exchange2D (sendbuf, recvbuf, nx,hx, ny,hy)
 !=====================================================================
 !
