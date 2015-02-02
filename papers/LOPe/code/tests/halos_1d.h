ae63554cdb0ef0b6c8af60a586b4c917f8363052
Craig Rasmussen <rasmus@uoregon.edu>
2015-02-02 10:27:43 -0800

    Changed to be testing version rather than performance measurements.

--- papers/LOPe/code/tests/halos_1d.h
+++ papers/LOPe/code/tests/halos_1d.h
@@ -1,31 +1,19 @@
 #define K1_ (get_global_id(0))
-#define K2_ (get_global_id(1))
 
 //#define N1_ (16*1024*1024)
-#define N1_ (32)
-#define N2_ (4)
+#define N1_ (16)
 
-#define A_H1L_ 1
-#define A_H1R_ 1
-#define A_H2L_ 1
-#define A_H2R_ 1
+#define A_H1L_ 2
+#define A_H1R_ 2
 #define A_S1_  1
-#define A_S2_  (A_H1L_ + A_H1R_ + N1_)
 
-
-#define B_H1L_ 1
-#define B_H1R_ 1
-#define B_H2L_ 1
-#define B_H2R_ 1
+#define B_H1L_ 2
+#define B_H1R_ 2
 #define B_S1_  1
-#define B_S2_  (B_H1L_ + B_H1R_ + N1_)
 
-#define C_H1L_ 1
-#define C_H1R_ 1
-#define C_H2L_ 1
-#define C_H2R_ 1
+#define C_H1L_ 2
+#define C_H1R_ 2
 #define C_S1_  1
-#define C_S2_  (C_H1L_ + C_H1R_ + N1_)
 
 // 1D halo size for the given SIDE of the array NAME
 #define HALO1(NAME,SIDE)   ( (NAME ## _H1 ## SIDE ## _) )
