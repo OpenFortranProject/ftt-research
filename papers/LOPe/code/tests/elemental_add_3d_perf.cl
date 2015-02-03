afeabf421d31226385236f496f8f8fcfea150b9e
rasmussn <rasmus@uoregon.edu>
2015-02-03 08:08:49 -0800

    Initial version for performance measurements.

---
+++ papers/LOPe/code/tests/elemental_add_3d_perf.cl
@@ -0,0 +1,102 @@
+#include "halos_3d.h"
+
+#define COPY_HALOS
+
+/**
+ * simple kernel that computes the sum of array elements
+ */
+__kernel void elemental_add_3d (
+    __global float * A,
+    __global float * B,
+    __global float * C, __global float * C_H_ )
+{
+   size_t hoff_, hsft_;
+
+   if (K1_ < N1_ && K2_ < N2_ && K3_ < N3_) {
+
+#ifdef COPY_HALOS
+      // copy incoming halo regions (to halo region of array)
+      // ----------------------------------------------------------------------------------------------
+     hoff_ = 0;
+     if (K1_ < HALO1(C,L)) {
+        C[IDX3(C,-HALO1(C,L),0,0)] = C_H_[hoff_ + K1_ + K2_*HALO1(C,L) + K3_*HALO1(C,L)*N2_];
+     }
+
+     hoff_ += HALO1(C,L) * N2_*N3_;
+     hsft_  = N1_ - HALO1(C,R);
+     if (K1_ >= hsft_) {
+        C[IDX3(C,HALO1(C,R),0,0)] = C_H_[hoff_ + (K1_ - hsft_) + K2_*HALO1(C,R) + K3_*HALO1(C,R)*N2_];
+     }
+
+     hoff_ += HALO1(C,R) * N2_*N3_;
+     if (K2_ < HALO2(C,L)) {
+        C[IDX3(C,0,-HALO2(C,L),0)] = C_H_[hoff_ + K1_ + K2_*N1_ + K3_*N1_*HALO2(C,L)];
+     }
+
+     hoff_ += HALO2(C,L) * N1_*N3_;
+     hsft_  = N2_ - HALO2(C,R);
+     if (K2_ >= hsft_) {
+        C[IDX3(C,0,HALO2(C,R),0)] = C_H_[hoff_ + K1_ + (K2_ - hsft_)*N1_ + K3_*N1_*HALO2(C,R)];
+     }
+
+     hoff_ += HALO2(C,R) * N1_*N3_;
+     if (K3_ < HALO3(C,L)) {
+        C[IDX3(C,0,0,-HALO3(C,L))] = C_H_[hoff_ + K1_ + K2_*N1_ + K3_*N1_*N2_];
+     }
+
+     hoff_ += HALO3(C,L) * N1_*N2_;
+     hsft_  = N3_ - HALO3(C,R);
+     if (K3_ >= hsft_) {
+        C[IDX3(C,0,0,HALO3(C,R))] = C_H_[hoff_ + K1_ + K2_*N1_ + (K3_ - hsft_)*N1_*N2_];
+     }
+
+     barrier (CLK_GLOBAL_MEM_FENCE);
+#endif
+
+
+     // run algorithm
+     // ----------------------------------------------------------------------------------------------
+
+      C[IDX3(C,0,0,0)] = A[IDX3(A,0,0,0)] + B[IDX3(B,0,1,0)];
+
+
+#ifdef COPY_HALOS
+      // copy outgoing halo regions
+      // ----------------------------------------------------------------------------------------------
+     hoff_ = 0;
+     if (K1_ < HALO1(C,L)) {
+        C_H_[hoff_ + K1_ + K2_*HALO1(C,L) + K3_*HALO1(C,L)*N2_] = C[IDX3(C,0,0,0)];
+     }
+
+     hoff_ += HALO1(C,L) * N2_*N3_;
+     hsft_  = N1_ - HALO1(C,R);
+     if (K1_ >= hsft_) {
+        C_H_[hoff_ + (K1_ - hsft_) + K2_*HALO1(C,R) + K3_*HALO1(C,R)*N2_] = C[IDX3(C,0,0,0)];
+     }
+
+     hoff_ += HALO1(C,R) * N2_*N3_;
+     if (K2_ < HALO2(C,L)) {
+        C_H_[hoff_ + K1_ + K2_*N1_ + K3_*N1_*HALO2(C,L)] = C[IDX3(C,0,0,0)];
+     }
+
+     hoff_ += HALO2(C,L) * N1_*N3_;
+     hsft_  = N2_ - HALO2(C,R);
+     if (K2_ >= hsft_) {
+        C_H_[hoff_ + K1_ + (K2_ - hsft_)*N1_ + K3_*N1_*HALO2(C,R)] = C[IDX3(C,0,0,0)];
+     }
+
+     hoff_ += HALO2(C,R) * N1_*N3_;
+     if (K3_ < HALO3(C,L)) {
+        C_H_[hoff_ + K1_ + K2_*N1_ + K3_*N1_*N2_] = C[IDX3(C,0,0,0)];
+     }
+
+     hoff_ += HALO3(C,L) * N1_*N2_;
+     hsft_  = N3_ - HALO3(C,R);
+     if (K3_ >= hsft_) {
+        C_H_[hoff_ + K1_ + K2_*N1_ + (K3_ - hsft_)*N1_*N2_] = C[IDX3(C,0,0,0)];
+     }
+#endif
+
+   }
+
+}
---
+++ papers/LOPe/code/tests/elemental_add_3d_perf.f90
@@ -0,0 +1,198 @@
+program elemental_add_3d
+!...TODO-GENERATE
+   use MPI_F08
+   use ForOpenCL
+   use Parallel_Halo , only : Context, Parallel_Start, Parallel_End, Parallel_Topology
+   use Parallel      , only : Halo_Exchange3D
+!...TODO-END-GENERATE
+   use Timer_mod
+   implicit none
+
+   ! layer size
+   integer(c_size_t), parameter :: NX  = 512
+   integer(c_size_t), parameter :: NY  = 256
+   integer(c_size_t), parameter :: NZ  = 256
+
+!...TODO-GENERATE
+   integer :: cl_status_
+
+   integer(c_size_t), parameter :: NHX = 1
+   integer(c_size_t), parameter :: NHY = 1
+   integer(c_size_t), parameter :: NHZ = 1
+
+   integer(c_size_t), parameter ::  SIZE_FLOAT = 4
+   integer(c_size_t), parameter :: N_HALO_ELEM = (2*NHX)*NY*NZ + NX*(2*NHY)*NZ + NX*NY*(2*NHZ)
+   integer(c_size_t), parameter ::   HALO_SIZE = N_HALO_ELEM*SIZE_FLOAT
+
+   ! work group size
+   integer(c_size_t), parameter :: NXL = 64
+   integer(c_size_t), parameter :: NYL = 2
+   integer(c_size_t), parameter :: NZL = 2
+   integer(c_size_t) :: nxGlobal, nyGlobal, nzGlobal, nxLocal, nyLocal, nzLocal
+
+   Type(Context) :: aContext
+   Integer       :: ndims, dims(3)
+!...TODO-END-GENERATE
+
+   real(c_float), target, allocatable, dimension(:,:,:) :: A, B, C
+
+   type(CPUTimer) :: timer
+   real(c_double) :: h_time
+
+   integer :: device, i, j, k, hoff, nLoops = 100, nWarm = 10
+
+!...TODO-GENERATE
+   real(c_float), allocatable :: in_C_H_(:), out_C_H_(:)
+
+   type(CLDevice) :: cl_device_
+   type(CLKernel) :: kernel
+   type(CLBuffer) :: cl_A_, cl_B_, cl_C_
+   type(CLBuffer) :: cl_C_H_
+
+   integer(c_size_t) :: mem_size = (NX+2*NHX) * (NY+2*NHY) * (NZ+2*NHZ) * SIZE_FLOAT
+
+   allocate(in_C_H_(N_HALO_ELEM), out_C_H_(N_HALO_ELEM))
+
+   Call Parallel_Start(aContext)
+
+   ndims = 1
+   dims  = [0,0,0]
+
+   Call Parallel_Topology(aContext, ndims, dims)
+!...TODO-END-GENERATE
+
+   device = 1 + aContext%rank
+   cl_status_ = init_device(cl_device_, device)
+
+   nxGlobal = NX; nyGlobal = NY; nzGlobal = NZ
+   if (device /= 0) then
+      nxLocal = NXL; nyLocal = NYL; nzLocal = NZL
+   else
+      nxLocal = 1; nyLocal = 1; nzLocal = 1
+   end if
+
+   allocate(A(1-NHX:NX+NHX,1-NHY:NY+NHY,1-NHZ:NZ+NHZ))
+   allocate(B(1-NHX:NX+NHX,1-NHY:NY+NHY,1-NHZ:NZ+NHZ))
+   allocate(C(1-NHX:NX+NHX,1-NHY:NY+NHY,1-NHZ:NZ+NHZ))
+
+   A = 0
+   B = 1
+   C = 0
+   do k = 1, NZ
+      do j = 1, NY
+         do i = 1, NX
+            A(i,j,k) = i + 100*j + 1000*k
+         end do
+      end do
+   end do
+
+    in_C_H_ = 0
+   out_C_H_ = 0
+
+   ! create memory buffers
+   !
+   cl_A_ = createBuffer(cl_device_, CL_MEM_READ_ONLY + CL_MEM_COPY_HOST_PTR, mem_size, c_loc(A))
+   cl_B_ = createBuffer(cl_device_, CL_MEM_READ_ONLY + CL_MEM_COPY_HOST_PTR, mem_size, c_loc(B))
+   cl_C_ = createBuffer(cl_device_, CL_MEM_READ_ONLY + CL_MEM_COPY_HOST_PTR, mem_size, c_loc(C))
+!   cl_C_ = createBuffer(cl_device_, CL_MEM_WRITE_ONLY, mem_size, C_NULL_PTR)
+
+   cl_C_H_ = createBuffer(cl_device_, CL_MEM_READ_WRITE+CL_MEM_COPY_HOST_PTR, HALO_SIZE, c_loc(out_C_H_))
+
+   !print *, mem_size, N_HALO_ELEM, HALO_SIZE
+
+
+   ! create the kernel
+   !
+   kernel = createKernel(cl_device_, "elemental_add_3d")
+
+   ! add arguments
+   !
+   cl_status_ = setKernelArgMem(kernel, 0, clMemObject(cl_A_))
+   cl_status_ = setKernelArgMem(kernel, 1, clMemObject(cl_B_))
+   cl_status_ = setKernelArgMem(kernel, 2, clMemObject(cl_C_))
+   cl_status_ = setKernelArgMem(kernel, 3, clMemObject(cl_C_H_))
+
+   ! run the kernel on the device
+   !
+   if (aContext%rank==0) print *
+   if (aContext%rank==0) print "('Measuring time to compute elemental add (NX,NY,NZ):',3I6)", NX, NY, NZ
+   call init(timer)
+   call start(timer)
+   do i = 1, nLoops
+      cl_status_ = writeBuffer(cl_C_H_, c_loc(out_C_H_), HALO_SIZE)
+      cl_status_ = run(kernel, nxGlobal, nyGlobal, nzGlobal, nxLocal, nyLocal, nzLocal)
+      cl_status_ = readBuffer(cl_C_H_, c_loc(in_C_H_), HALO_SIZE)
+      call Halo_Exchange3D (in_C_H_, out_C_H_, NX,NHX, NY,NHY, NZ,NHZ)
+   end do
+   cl_status_ = clFinish(kernel%commands)
+   call stop(timer)
+
+   h_time = elapsed_time(timer)
+   print *, aContext%rank, ": host time ==", real(h_time)/nLoops, "ms per iteration"
+   call MPI_Barrier(MPI_COMM_WORLD)
+
+   ! get the results
+   !
+   cl_status_ = readBuffer(cl_C_, c_loc(C), mem_size)
+   cl_status_ = readBuffer(cl_C_H_, c_loc(in_C_H_), HALO_SIZE)
+
+!   print *
+!   print *, "------------------"
+!   hoff = 0
+!   print *, in_C_H_(hoff+1:NHX*NY*NZ)                             ! left
+!   print *
+!   hoff = hoff + NHX*NY*NZ
+!   print *, in_C_H_(hoff+1:2*NHX*NY*NZ)                           ! right
+!   print *, "------------------"
+!   hoff = hoff + NHX*NY*NZ
+!   print *, in_C_H_(hoff+1:2*NHX*NY*NZ+NHY*NX*NZ)                 ! bottom
+!   print *
+!   hoff = hoff + NHY*NX*NZ
+!   print *, in_C_H_(hoff+1:2*NHX*NY*NZ+2*NHY*NX*NZ)               ! top
+!   print *, "------------------"
+!   hoff = hoff + NHY*NX*NZ
+!   print *, in_C_H_(hoff+1:2*NHX*NY*NZ+2*NHY*NX*NZ+NHZ*NX*NY)     ! front
+!   print *, "------------------"
+!   hoff = hoff + NHZ*NX*NY
+!   print *, in_C_H_(hoff+1:2*NHX*NY*NZ+2*NHY*NX*NZ+2*NHZ*NX*NY)   ! back
+!   print *, "------------------"
+
+!   k = 4
+!   print *
+!   print *, C(:,0,k)
+!   print *, "------------------"
+!   print *, C(:,1,k)
+!   print *, "------------------"
+!   print *, C(:,2,k)
+!   print *, "------------------"
+!   print *, C(:,3,k)
+!   print *, "------------------"
+!   print *, C(:,4,k)
+!   print *, "------------------"
+!   print *, C(:,5,k)
+!   print *, "------------------"
+!   print *
+
+   do k = 1, nz
+      do j = 1, ny
+         do i = 1, nx
+            if (C(i,j,k) /= A(i,j,k) + B(i,j,k)) then
+               print *, aContext%rank, ": Results incorrect at ", i, j, k
+               goto 99
+            end if
+         end do
+      end do
+   end do
+
+   if (cl_status_ == CL_SUCCESS) then
+      print *, aContext%rank, ": Correctness verified..."
+   end if
+   !print *
+
+   deallocate(A, B, C)
+
+!...TODO-GENERATE
+99 Call Parallel_End(aContext)
+!...TODO-END-GENERATE
+
+end program
