1e44624593dd9dc26e323af5f24f6a5759706939
rasmussn <rasmus@uoregon.edu>
2015-12-18 08:53:11 -0800

    Initial version.

---
+++ papers/CAFe/code/MPI/multigrid.f90
@@ -0,0 +1,90 @@
+Module MultiGrid
+!
+! old grid with HALO(-1:*:1), DIMENSION(0:N)
+!
+!  x |          o          | x    N/4-1 interior  N/4+1  total  [N=8]
+!  x |    o     o     o    | x    N/2-1 interior  N/2+1  total  [N=8]
+!  x | o  o  o  o  o  o  o | x    N-1   interior  N  +1  total  [N=8]
+!
+!    |       x| ^ |x      ???       N interior N+2 total for one parallel node (boundary shared)
+!         proc boundary   ???       proc boundary is shared point
+!
+
+! new grid with HALO(-2:*:2), DIMENSION(-1:N+1)
+!
+! N-1, N/2-1,N/4-1,... interior cells, N+3,N/2+3,N/4+3,... total cells
+!
+!  x           x |          o          | o |          o          | x           x
+!        x     x |    o     o     o    | o |    o     o     o    | x     x
+!           x  x | o  o  o  o  o  o  o | o | o  o  o  o  o  o  o | x  x
+!  
+!  N=8, NP=2
+!
+!  N-1 interior, 4 halo cells with N+3 total points
+!    - one cell on each boundary shared with other processors
+!    - one halo cell at each end
+!    - could treat first border cell as interior for   
+!
+  
+  real, parameter :: PI = 4.0d0*atan(1.0d0)
+
+Contains
+
+Subroutine AddFourierMode(N, V, k)
+!
+! Add Fourier mode k to V
+!
+  implicit none
+  integer, intent(in   )  ::  N, k
+  real,    intent(inout)  ::  V(-1:N+1)  ! includes boundaries at -1,0:N,N+1
+  integer :: i
+
+  do i = -1, N+1
+    V(i) = V(i) + sin(i*k*PI/N)
+  end do
+
+End Subroutine AddFourierMode
+
+Subroutine Prolongate(N, V1h, V2h)
+!
+!  Interpolation (prolongation) operator R^(N/2+1) => R^(N+1)
+!
+!  N-1 is the number of interior fine grid cells
+!
+  implicit none
+  integer, intent(in) :: N
+  real :: V1h(-1:N+1), V2h(-1:N/2+1)
+  integer :: i, ii, m
+
+  m = N/2 - 1     ! # interior coarse cells
+
+  do i = 0, m
+     ii = 2*i
+     V1h(ii)   = V2h(i)
+     V1h(ii+1) = 0.5*(V2h(i) + V2h(i+1))
+  end do
+  V1h(N) = V2h(m+1)      ! right halo cell
+
+End Subroutine Prolongate
+
+Subroutine Restrict(N, V1h, V2h)
+!
+!  Restriction operator R^(N+1) => R^(N/2+1)
+!
+!  N-1 is the number of interior fine grid cells
+!
+  implicit none
+  integer, intent(in) :: N
+  real :: V1h(-1:N+1), V2h(-1:N/2+1)
+  integer :: i, ii, m
+
+  m = N/2 - 1     ! # interior coarse cells
+
+  do i = 0, m+1
+     ii = 2*i
+     V2h(i) = 0.25*(V1h(ii-1) + 2.0*V1h(ii) + V1h(ii+1))
+  end do
+
+End Subroutine Restrict
+
+End Module MultiGrid
---
+++ papers/CAFe/code/MPI/poisson-multigrid.F90
@@ -0,0 +1,146 @@
+Program PoissonMultigrid
+!=============================================================================
+! Name        : poisson-multigrid.F90
+! Version     :
+! Description : Solves the Poisson equation in one dimension.
+!
+! Method :
+!
+!   Multigrid.
+!
+!=============================================================================
+Use MultiGrid, only : AddFourierMode
+Use IO,        only : Textual_Output
+!Use Jacobi,   only : Iterate, Iterate1D
+!Use Jacobi,   only : Iterate1D
+Use MultiGrid, only : Restrict, Prolongate
+
+Implicit None
+
+!Real, parameter :: w = 1.0
+Real, parameter :: w = (2.0/3.0)
+Logical :: first = .true.
+
+Integer, parameter :: NDIMS  =   1
+Integer, parameter :: N      =  64
+Integer, parameter :: fd     =  12
+
+Integer :: i
+Integer :: nsteps = 10
+
+Real, allocatable :: V1h(:), Tmp(:), V2h(:), V4h(:), V8h(:)
+
+Allocate(V1h(-1:N+1), Tmp(-1:N+1))
+Allocate(V2h(-1:N/2+1))
+Allocate(V4h(-1:N/4+1))
+Allocate(V8h(-1:N/8+1))
+
+open(unit=fd, file="error_time.dat")
+
+!! Initialize
+!
+V1h = 0.0
+Call AddFourierMode(N, V1h,  1)
+Call AddFourierMode(N, V1h,  6)
+Call AddFourierMode(N, V1h, 16)
+V1h = (1./3.)*V1h
+
+!... Relax solution on 1h mesh
+!    -------------------------
+Tmp = 0.0
+Call Textual_Output(N, V1h, "1h_0")
+do i = 2, nsteps, 2
+  Call Relax(N, V1h, Tmp)      ; Call Exchange_Halo(N, Tmp)
+  Call Relax(N, Tmp, V1h)      ; Call Exchange_Halo(N, V1h)
+  write(fd, *) i, maxval(V1h)
+end do
+Call Textual_Output(N, V1h, "1h_mid")
+
+!... Relax solution on 2h mesh
+!    -------------------------
+Tmp = 0.0
+Call Restrict(N, V1h, V2h)
+Call Textual_Output(N/2, V2h, "2h_0")
+do i = 2, nsteps, 2
+  Call Relax(N/2, V2h, Tmp)      ; Call Exchange_Halo(N/2, Tmp)
+  Call Relax(N/2, Tmp, V2h)      ; Call Exchange_Halo(N/2, V2h)
+  write(fd, *) i, maxval(V2h)
+end do
+Call Textual_Output(N/2, V2h, "2h_mid")
+
+!... Relax solution on 4h mesh
+!    -------------------------
+Tmp = 0.0
+Call Restrict(N/2, V2h, V4h)
+Call Textual_Output(N/4, V4h, "4h_0")
+do i = 2, nsteps, 2
+  Call Relax(N/4, V4h, Tmp)      ; Call Exchange_Halo(N/4, Tmp)
+  Call Relax(N/4, Tmp, V4h)      ; Call Exchange_Halo(N/4, V4h)
+  write(fd, *) i, maxval(V4h)
+end do
+Call Textual_Output(N/4, V4h, "4h_mid")
+
+!... Relax solution on 8h mesh
+!    -------------------------
+Tmp = 0.0
+Call Restrict(N/4, V4h, V8h)
+Call Textual_Output(N/8, V8h, "8h_0")
+do i = 2, nsteps, 2
+  Call Relax(N/8, V8h, Tmp)      ; Call Exchange_Halo(N/8, Tmp)
+  Call Relax(N/8, Tmp, V8h)      ; Call Exchange_Halo(N/8, V8h)
+  write(fd, *) i, maxval(V8h)
+end do
+Call Textual_Output(N/8, V8h, "8h_mid")
+
+!! IMPORTANT: this last step should be an exact solution on a smaller grid probably
+!
+do i = 2, 5*nsteps, 2
+  Call Relax(N/8, V8h, Tmp)      ; Call Exchange_Halo(N/8, Tmp)
+  Call Relax(N/8, Tmp, V8h)      ; Call Exchange_Halo(N/8, V8h)
+  write(fd, *) i, maxval(V8h)
+end do
+Call Textual_Output(N/8, V8h, "8h_end")
+
+!! IMPORTANT: probably should relax on the way back down the grids as well
+!
+Call Prolongate    (N/4, V4h, V8h)
+Call Textual_Output(N/4, V4h, "4h_end")
+
+Call Prolongate    (N/2, V2h, V4h)
+Call Textual_Output(N/2, V2h, "2h_end")
+
+Call Prolongate    (N,   V1h, V2h)
+Call Textual_Output(N,   V1h, "1h_end")
+
+close(fd)
+
+CONTAINS
+
+Pure Subroutine Relax(N, A, Adv)
+!
+! Relax on the interior and the two halo cells shared with the left and right neighbors
+!   - shared halo cells are computed twice and are not exchanged
+!   - the outside halo cells are from neighbors and cannot be not computed
+!
+   Implicit None
+   Integer, intent( in) :: N
+   Real,    intent( in) :: A  (-1:N+1)
+   Real,    intent(out) :: Adv(-1:N+1)
+   Integer              :: i
+
+   do i = 0, N
+      Adv(i) = (1.0 - w)*A(i) + 0.5*w*(A(i-1) + A(i+1))
+   end do
+
+End Subroutine Relax
+
+Subroutine Exchange_Halo(N, A)
+!
+! Exchange halo information between neighboring processes
+!
+   Implicit None
+   Integer, intent( in) :: N
+   Real,    intent( in) :: A  (-1:N+1)
+End Subroutine Exchange_Halo
+
+End Program PoissonMultigrid
