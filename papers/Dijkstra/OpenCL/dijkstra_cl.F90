4fa396e255762e3aa900162218909b5ae0da8212
Soren Rasmussen <s.c.rasmussen@gmail.com>
2016-02-01 15:04:07 -0800

    Fixed gws and lws

--- papers/Dijkstra/OpenCL/dijkstra_cl.F90
+++ papers/Dijkstra/OpenCL/dijkstra_cl.F90
@@ -4,9 +4,9 @@ USE MPI_f08
 USE forward_star
 IMPLICIT NONE
 
-INTEGER, PARAMETER :: NX = 8
-INTEGER, PARAMETER :: NY = 8
-INTEGER, PARAMETER :: NZ = 1
+INTEGER, PARAMETER :: NX = 128
+INTEGER, PARAMETER :: NY = 128
+INTEGER, PARAMETER :: NZ = 32
 INTEGER, PARAMETER :: NFS = 818
 REAL, PARAMETER :: VERY_BIG = huge(1.0)/10.0
 
@@ -18,7 +18,8 @@ INTEGER, ALLOCATABLE, TARGET, DIMENSION(:,:)   :: Offset
 DOUBLE PRECISION :: time, time_sweep = 0.0d0, time_reduce = 0.0d0
 INTEGER :: i, j, k
 LOGICAL :: done = .FALSE.
-LOGICAL :: debug = .TRUE.
+! LOGICAL :: debug = .TRUE.
+LOGICAL :: debug = .FALSE.
 
 INTEGER :: dev
 INTEGER :: ocl_id
@@ -35,7 +36,9 @@ INTEGER(KIND=cl_int) :: cl_status__
 INTEGER(KIND=c_size_t) :: cl_size__
 INTEGER(KIND=c_size_t) :: cl_gwo__(3)
 INTEGER(KIND=c_size_t) :: cl_gws__(3)
-INTEGER(KIND=c_size_t) :: cl_lws__(3) = [16,8,1]
+INTEGER(KIND=c_size_t) :: cl_lws__(3) = [128,1,1]
+INTEGER :: stepsTaken = 0
+REAL    :: bandwidth  
 
 ocl_id = 1
 dev = get_subimage(ocl_id,cl_dev_)
@@ -62,7 +65,7 @@ CALL read_forward_star(NFS,Offset)
 U = 1.0
 TT = VERY_BIG
 
-i = 1;  j = 1;  k = 1
+i = NX/2;  j = NY/2;  k = NZ/2
 TT(i,j,k) = 0.0
 
 cl_size__ = 4*NX*NY*NZ
@@ -90,7 +93,7 @@ DO WHILE(.NOT. done)
   time = MPI_Wtime()
 
   cl_gwo__ = [0,0,0]
-  cl_gws__ = [16,16,4]
+  cl_gws__ = [NX,NY,NZ]
 
   cl_status__ = run(cl_sweep_,3,cl_gwo__,cl_gws__,cl_lws__)
   cl_status__ = clFinish(cl_sweep_%commands)
@@ -102,7 +105,8 @@ DO WHILE(.NOT. done)
   IF (sum(Changed) == 0) done = .TRUE.
   time_reduce = time_reduce + MPI_Wtime() - time
 
-  PRINT *, "# changed:",sum(Changed)
+  stepsTaken = stepsTaken + 1
+  ! PRINT *, "# changed:", sum(Changed), "step", stepsTaken
 
   ! done = .true.
 
@@ -111,19 +115,25 @@ END DO
 cl_size__ = 4*NX*NY*NZ
 cl_status__ = readBuffer(cl_TT_,C_LOC(TT),cl_size__)
 
+open(unit = 7, file = "output.txt")
+
 IF (debug) THEN
   PRINT *, ''
-  DO i = 1, NX
+  DO k = 1, NZ
      DO j = 1, NY
-        DO k = 1, NZ
+        DO i = 1, NX
            PRINT *, i,j,k,TT(i,j,k)
+           ! write (7,*), i,j,k,TT(i,j,k)
         END DO
      END DO
   END DO 
 END IF
+close(7)
 
 PRINT *, ''
 PRINT *, "Sweep/reduce time for N=", NX*NY*NZ, real(time_sweep), real(time_reduce)
+bandwidth = stepsTaken*4.0*NX*NY*NZ*NFS*2.0 / (real(time_sweep) * 1000000000)
+PRINT *, "Steps Taken", stepsTaken, "Bandwidth", bandwidth
 
 DEALLOCATE(U,TT,Changed,Offset)
 
