b65138ea17f3073ac33cd7c60a9763bd87e3b7d2
Soren Rasmussen <s.c.rasmussen@gmail.com>
2016-02-03 17:51:52 -0800

    Added double buffering to the Fortran code

--- papers/Dijkstra/OpenCL/dijkstra_cl.F90
+++ papers/Dijkstra/OpenCL/dijkstra_cl.F90
@@ -28,6 +28,7 @@ INTEGER :: ocl_id
 TYPE(CLDevice) :: cl_dev_
 TYPE(CLBuffer) :: cl_U_
 TYPE(CLBuffer) :: cl_TT_
+TYPE(CLBuffer) :: cl_TTBuf_
 TYPE(CLBuffer) :: cl_Changed_
 TYPE(CLBuffer) :: cl_Offset_
 TYPE(CLKernel) :: cl_sweep_
@@ -38,7 +39,7 @@ INTEGER(KIND=c_size_t) :: cl_size__
 INTEGER(KIND=c_size_t) :: cl_gwo__(3)
 INTEGER(KIND=c_size_t) :: cl_gws__(3)
 INTEGER(KIND=c_size_t) :: cl_lws__(3)
-INTEGER :: stepsTaken = 0, rightHalo
+INTEGER :: stepsTaken = 0, rightHalo, change
 REAL :: var, bandwidth  
 
 ocl_id = 1
@@ -50,11 +51,15 @@ cl_sweep_ = createKernel(cl_dev_,"sweep")
 
 open(unit = 2, file = "velocity-241-241-51-nonConst.txt")
 read (2,*), NX, NY, NZ
+! NX = 4
+! NY = 4
+! NZ = 4
+
 newNX = 256
 newNY = 256
 newNZ = NZ
 rightHalo = newNX - NX
-cl_lws__ = [128, 1, 1]
+cl_lws__ = [8, 8, 1]
 print *, "   NX,    NY,    NZ", NX, NY, NZ
 print *, "newNX, newNY, newNZ", newNX, newNY, newNZ, "cl_lws__", cl_lws__
 
@@ -74,9 +79,11 @@ DO k = 1, NZ
    END DO
 END DO
 
+! print *,U
 cl_size__ = 4*newNX*newNY*newNZ
 cl_U_  = createBuffer(cl_dev_,CL_MEM_READ_WRITE,cl_size__,C_NULL_PTR)
 cl_TT_ = createBuffer(cl_dev_,CL_MEM_READ_WRITE,cl_size__,C_NULL_PTR)
+cl_TTBuf_ = createBuffer(cl_dev_,CL_MEM_READ_WRITE,cl_size__,C_NULL_PTR)
 cl_Changed_ = createBuffer(cl_dev_,CL_MEM_READ_WRITE,cl_size__,C_NULL_PTR)
 print *, "cl_U, cl_TT, cl_Changed size = ", cl_size__
 cl_size__ = 4*3*NFS
@@ -93,6 +100,7 @@ TT(i,j,k) = 0.0
 cl_size__ = 4*newNX*newNY*newNZ
 cl_status__ = writeBuffer(cl_U_, C_LOC(U ),cl_size__)
 cl_status__ = writeBuffer(cl_TT_,C_LOC(TT),cl_size__)
+cl_status__ = writeBuffer(cl_TTBuf_,C_LOC(TT),cl_size__)
 cl_status__ = writeBuffer(cl_Changed_,C_LOC(Changed),cl_size__)
 cl_size__ = 4*3*NFS
 cl_status__ = writeBuffer(cl_Offset_,C_LOC(Offset),cl_size__)
@@ -107,8 +115,12 @@ cl_status__ = setKernelArg(cl_sweep_,6,clMemObject(cl_Offset_))
 cl_status__ = setKernelArg(cl_sweep_,7,clMemObject(cl_Changed_))
 cl_status__ = setKernelArg(cl_sweep_,8,rightHalo)
 cl_status__ = setKernelArg(cl_sweep_,9,stepsTaken)
+cl_status__ = setKernelArg(cl_sweep_,10,clMemObject(cl_TTBuf_))
 
-DO WHILE(.NOT. done)
+
+change = 0
+! DO WHILE(stepsTaken .lt. 6)
+DO WHILE(.not. done)
   time = MPI_Wtime()
 
   cl_gwo__ = [0,0,0]
@@ -121,28 +133,37 @@ DO WHILE(.NOT. done)
   time = MPI_Wtime()
   cl_size__ = 4*NX*NY*NZ
   cl_status__ = readBuffer(cl_Changed_,C_LOC(Changed),cl_size__)
-  IF (sum(Changed) == 0) done = .TRUE.
-  stepsTaken = stepsTaken + 1
+  IF (sum(Changed) .le. 0) done = .TRUE.
   cl_status__ = setKernelArg(cl_sweep_, 9,stepsTaken)
+  stepsTaken = stepsTaken + 1
   time_reduce = time_reduce + MPI_Wtime() - time
 
   PRINT *, "# changed:", sum(Changed), "step", stepsTaken
+  ! if (done .eq. .TRUE. .and. change .eq. 0) then
+  !    done = .FALSE.
+  !    change = 1
+  ! end if
   ! done = .true.
 
 END DO 
 
 cl_size__ = 4*newNX*newNY*newNZ
-cl_status__ = readBuffer(cl_TT_,C_LOC(TT),cl_size__)
+if (mod(stepsTaken, 2) .eq. 0) then
+   cl_status__ = readBuffer(cl_TT_,C_LOC(TT),cl_size__)
+else
+   cl_status__ = readBuffer(cl_TTBuf_,C_LOC(TT),cl_size__)
+end if
 
 open(unit = 7, file = "output.txt")
 
 IF (debug) THEN
   PRINT *, ''
-  DO k = 1, NZ
-     DO j = 1, NY
-        DO i = 1, NX
-           PRINT *, i,j,k,TT(i,j,k)
+  DO k = 1, newNZ
+     DO j = 1, newNY
+        DO i = 1, newNX
+           ! PRINT *, i,j,k,TT(i,j,k)
            ! write (7,*), i,j,k,TT(i,j,k)
+           write (7,*), TT(i,j,k)
         END DO
      END DO
   END DO 
@@ -158,6 +179,7 @@ DEALLOCATE(U,TT,Changed,Offset)
 
 cl_status__ = releaseMemObject(cl_U_)
 cl_status__ = releaseMemObject(cl_TT_)
+cl_status__ = releaseMemObject(cl_TTBuf_)
 cl_status__ = releaseMemObject(cl_Changed_)
 cl_status__ = releaseMemObject(cl_Offset_)
 
