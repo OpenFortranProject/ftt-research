d08e5a785effb2ca07b36ed767af6b1a1cc4d548
Craig Rasmussen <rasmus@uoregon.edu>
2016-02-05 15:59:06 -0800

    Added read_velocity_model.F90

--- papers/Dijkstra/OpenCL/forward_star.F90
+++ papers/Dijkstra/OpenCL/forward_star.F90
@@ -22,5 +22,24 @@ subroutine read_forward_star(nfs, off, dist)
 
 end subroutine read_forward_star
 
+subroutine read_velocity_model(nx, ny, nz, U)
+  integer, intent(in ) :: nx, ny, nz
+  real,    intent(out) :: U(nx,ny,nz)
+  integer :: i, j, k, oi, oj, ok
+
+  open(unit=1, file='srModel_forTomographyPaper.txt')
+
+  do i = 1, nx
+     do j = 1, ny
+        do k = 1, nz
+           read(1, *) oi, oj, ok, U(i,j,k)
+        end do
+     end do
+  end do
+
+  close(unit=1)
+
+end subroutine read_velocity_model
+
 end module forward_star
 
