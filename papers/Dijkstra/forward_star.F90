c9c26e61cb1a225e7068fe5e06451a138341d929
Craig Rasmussen <rasmus@uoregon.edu>
2016-02-05 16:03:22 -0800

    Added read_velocity_model.F90 and distance calculation to forward star read.

--- papers/Dijkstra/forward_star.F90
+++ papers/Dijkstra/forward_star.F90
@@ -2,20 +2,44 @@ module forward_star
 
 contains
 
-subroutine read_forward_star(nfs, off)
+subroutine read_forward_star(nfs, off, dist)
   integer, intent(in ) :: nfs
   integer, intent(out) :: off(3,nfs)
-  integer :: i
+  real,    intent(out) :: dist(nfs)
+  integer :: i, oi, oj, ok
 
   open(unit=1, file='arcList.txt')
 
   do i = 1, nfs
-     read(1, *) off(1,i), off(2,i), off(3,i)
+     read(1, *) oi, oj, ok
+     off(1,i) = oi
+     off(2,i) = oj
+     off(3,i) = ok
+     dist(i) = sqrt( REAL(oi*oi + oj*oj + ok*ok) );
   end do
 
   close(unit=1)
 
 end subroutine read_forward_star
+
+subroutine read_velocity_model(nx, ny, nz, U)
+  integer, intent(in ) :: nx, ny, nz
+  real,    intent(out) :: U(nx,ny,nz)
+  integer :: i, j, k, oi, oj, ok
+
+  open(unit=1, file='srModel_forTomographyPaper.txt')
+
+  do i = 1, nx
+     do j = 1, ny
+        do k = 1, nz
+           read(1, *) oi, oj, ok, U(i,j,k)
+        end do
+     end do
+  end do
+
+  close(unit=1)
+
+end subroutine read_velocity_model
 
 end module forward_star
 
