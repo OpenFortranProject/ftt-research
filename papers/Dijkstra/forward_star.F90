10ee2f76db1657f9b7ea500d7fb85e98c514bb9c
Craig Rasmussen <rasmus@uoregon.edu>
2016-02-05 16:15:47 -0800

    Added write_results.

--- papers/Dijkstra/forward_star.F90
+++ papers/Dijkstra/forward_star.F90
@@ -41,5 +41,20 @@ subroutine read_velocity_model(nx, ny, nz, U)
 
 end subroutine read_velocity_model
 
+subroutine write_results(nx, ny, nz, tt)
+  integer, intent(in ) :: nx, ny, nz
+  integer, intent(out) :: tt(nx,ny,nz)
+  integer :: i, j, k
+
+  open(unit=1, file='output.tt')
+
+  do i = 1, nfs
+     read(1, *) off(1,i), off(2,i), off(3,i)
+  end do
+
+  close(unit=1)
+
+end subroutine read_forward_star
+
 end module forward_star
 
