6b15c1cb55a1d7232242ab1392fbaf4256cc26df
rasmussn <rasmus@uoregon.edu>
2016-02-05 17:00:30 -0800

    Added calc_distance.

--- papers/Dijkstra/OpenCL/forward_star.F90
+++ papers/Dijkstra/OpenCL/forward_star.F90
@@ -2,20 +2,15 @@ module forward_star
 
 contains
 
-subroutine read_forward_star(nfs, off, dist)
+subroutine read_forward_star(nfs, off)
   integer, intent(in ) :: nfs
   integer, intent(out) :: off(3,nfs)
-  real,    intent(out) :: dist(nfs)
-  integer :: i, oi, oj, ok
+  integer :: i
 
   open(unit=1, file='arcList.txt')
 
   do i = 1, nfs
-     read(1, *) oi, oj, ok
-     off(1,i) = oi
-     off(2,i) = oj
-     off(3,i) = ok
-     dist(i) = sqrt( REAL(oi*oi + oj*oj + ok*ok) );
+     read(1, *) off(1,i), off(2,i), off(3,i)
   end do
 
   close(unit=1)
@@ -40,6 +35,36 @@ subroutine read_velocity_model(nx, ny, nz, U)
   close(unit=1)
 
 end subroutine read_velocity_model
+
+subroutine calc_distance(nfs, off, dist)
+  integer, intent(in ) :: nfs
+  integer, intent(in ) :: off(3,nfs)
+  real,    intent(out) :: dist(nfs)
+  integer :: i, oi, oj, ok
+
+  do i = 1, nfs
+     oi = off(1,i)
+     oj = off(2,i)
+     ok = off(3,i)
+     dist(i) = sqrt( REAL(oi*oi + oj*oj + ok*ok) );
+  end do
+
+end subroutine calc_distance
+
+subroutine write_results(nx, ny, nz, tt)
+  integer, intent(in ) :: nx, ny, nz
+  real,    intent(out) :: tt(nx,ny,nz)
+  integer :: i, j, k
+
+  open(unit=1, file='output.tt')
+
+  do i = 1, nfs
+     write (1,'(3i4,f8.3)'), i,j,k, tt(i,j,k)
+  end do
+
+  close(unit=1)
+
+end subroutine write_results
 
 end module forward_star
 
