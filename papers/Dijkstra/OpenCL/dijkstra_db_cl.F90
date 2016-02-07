4ef0b04dbec2aafacf5758290c2c80a21fe4232e
Craig Rasmussen <rasmus@uoregon.edu>
2016-02-06 19:48:59 -0800

    Added capability of multiple starting points.

--- papers/Dijkstra/OpenCL/dijkstra_db_cl.F90
+++ papers/Dijkstra/OpenCL/dijkstra_db_cl.F90
@@ -4,14 +4,23 @@ USE MPI_f08
 USE forward_star
 IMPLICIT NONE
 
-!! WARNING: current must also change (NX,NY,NZ) in sweep.cl
+character(len=3), parameter :: grid_name = '200'
+
+!! WARNING: current must also change (NX,NY,NZ) in sweep.cl (maybe)
 !
-INTEGER :: nx = 241
-INTEGER :: ny = 241
-INTEGER :: nz = 51
-INTEGER :: DB = 2
-INTEGER :: newNX, newNY, newNZ
-INTEGER, PARAMETER :: NFS = 818
+INTEGER :: nx = 200
+INTEGER :: ny = 200
+INTEGER :: nz = 200
+INTEGER :: newNX = 224
+INTEGER :: newNY = 224
+INTEGER :: newNZ = 224
+
+INTEGER, PARAMETER :: LWSX  = 224
+
+INTEGER, PARAMETER :: DB   = 1
+INTEGER, PARAMETER :: NPTS = 12
+INTEGER, PARAMETER :: NFS  = 818
+
 REAL, PARAMETER :: VERY_BIG = huge(1.0)/10.0
 
 REAL,    ALLOCATABLE, TARGET, DIMENSION(:,:,:,:) :: TT
@@ -20,7 +29,7 @@ INTEGER, ALLOCATABLE, TARGET, DIMENSION(:,:,:)   :: Changed
 INTEGER, ALLOCATABLE, TARGET, DIMENSION(:,:)     :: Offset
 REAL,    ALLOCATABLE, TARGET, DIMENSION(:)       :: Dist
 
-DOUBLE PRECISION :: time, time_diff, time_sweep = 0.0d0, time_reduce = 0.0d0
+DOUBLE PRECISION :: time, time0, time_diff, time_sweep = 0.0d0, time_total = 0.0d0
 INTEGER :: i, j, k
 LOGICAL :: done = .FALSE.
 LOGICAL :: debug = .TRUE.
@@ -42,7 +51,7 @@ INTEGER(KIND=c_size_t) :: cl_size__
 INTEGER(KIND=c_size_t) :: cl_gwo__(3)
 INTEGER(KIND=c_size_t) :: cl_gws__(3)
 INTEGER(KIND=c_size_t) :: cl_lws__(3)
-INTEGER :: stepsTaken, rightHalo, change, startPt(3)
+INTEGER :: stepsTaken, totalSteps, change, pt, startPt(3,12)
 REAL :: bandwidth  
 
 ocl_id = 1
@@ -52,12 +61,8 @@ dev = get_subimage(ocl_id,cl_dev_)
 cl_status__ = query(cl_dev_)
 cl_sweep_ = createKernel(cl_dev_,"sweep_db")
 
-newNX = 256
-newNY = 256
-newNZ = 64
-rightHalo = newNX - nx     ! WARNING: assert (newNX == newNY) as only one rightHalo used
 print *, "   nx,    ny,    nz", nx, ny, nz
-print *, "newNX, newNY, newNZ", newNX, newNY, newNZ, "cl_lws__", cl_lws__
+print *, "newNX, newNY, newNZ", newNX, newNY, newNZ
 
 ALLOCATE(U(newNX,newNY,newNZ))
 ALLOCATE(TT(newNX,newNY,newNZ,DB))
@@ -77,22 +82,11 @@ cl_size__ = 4*NFS
 cl_Dist_ = createBuffer(cl_dev_,CL_MEM_READ_ONLY,cl_size__,C_NULL_PTR)
 print *, "cl_Offset = ", cl_size__*3, cl_size__
 
+call read_starting_points(grid_name, NPTS, startPt)
+call read_velocity_model_padded(grid_name, newNX,newNY,newNZ, nx,ny,nz, U)
 call read_forward_star(NFS, Offset)
-call read_velocity_model_padded(newNX,newNY,newNZ, nx,ny,nz, U)
-
 Changed(:,:,:) = 0
-TT = VERY_BIG
 
-i = nx/2 + 1;  j = ny/2 + 1;  k = 1
-TT(i,j,k,:) = 0.0
-
-#ifdef INIT_AXES
-startPt(1) = i;  startPt(2) = j;  startPt(3) = k;
-call calc_linear_paths_padded(newNX, newNY, newNZ, nx, ny, nz, NFS, U, startPt, TT)
-#endif
-
-cl_size__ = DB * 4*newNX*newNY*newNZ
-cl_status__ = writeBuffer(cl_TT_,C_LOC(TT),cl_size__)
 cl_size__ = 4*newNX*newNY*newNZ
 cl_status__ = writeBuffer(cl_U_, C_LOC(U ),cl_size__)
 cl_status__ = writeBuffer(cl_Changed_,C_LOC(Changed),cl_size__)
@@ -111,56 +105,85 @@ cl_status__ = setKernelArg(cl_sweep_,6,clMemObject(cl_Offset_))
 cl_status__ = setKernelArg(cl_sweep_,7,clMemObject(cl_Changed_))
 cl_status__ = setKernelArg(cl_sweep_,8,stepsTaken)
 
-change = 0
-stepsTaken = 0
+cl_gwo__ = [0,0,0]
+cl_lws__ = [LWSX, 1, 1]
+cl_gws__ = [newNX,newNY,newNZ]
 
-DO WHILE(.not. done)
+print *, "cl_lws__", cl_lws__
+print *, "cl_gws__", cl_gws__
 
-  time = MPI_Wtime()
+totalSteps = 0
 
-  cl_gwo__ = [0,0,0]
-  cl_lws__ = [64, 1, 1]
-  cl_gws__ = [newNX,newNY,newNZ]
+time0 = MPI_Wtime()
 
-  cl_status__ = run(cl_sweep_,3,cl_gwo__,cl_gws__,cl_lws__)
-  cl_status__ = clFinish(cl_sweep_%commands)
-  time_diff = time_sweep
-  time_sweep = time_sweep + MPI_Wtime() - time
-  time_diff = time_sweep - time_diff
+do pt = 1, NPTS
 
-  time = MPI_Wtime()
-  cl_size__ = 4*newNX*newNY*newNZ
-  cl_status__ = readBuffer(cl_Changed_,C_LOC(Changed),cl_size__)
-  IF (sum(Changed) .le. 0) done = .TRUE.
-  cl_status__ = setKernelArg(cl_sweep_, 8, stepsTaken)
-  stepsTaken = stepsTaken + 1
-  time_reduce = time_reduce + MPI_Wtime() - time
+  change = 0
+  stepsTaken = 0
 
-  print *, "# changed:", sum(Changed), "step", stepsTaken, real(time_diff)
-  if (done .eq. .TRUE. .and. change .lt. 3) then
-     done = .FALSE.
-     change = change + 1
-  end if
+  !! initialize travel times
+  !
 
-END DO 
+  i = startPt(1,pt)
+  j = startPt(2,pt)
+  k = startPt(3,pt)
+  TT = VERY_BIG
+  TT(i,j,k,:) = 0.0
 
-cl_size__ = DB * 4*newNX*newNY*newNZ
+  print *
+  print *, "starting point", pt, ':', i, j, k
+  print *, "----------------------------------------------------------------"
+
+  cl_size__ = DB * 4*newNX*newNY*newNZ
+  cl_status__ = writeBuffer(cl_TT_,C_LOC(TT),cl_size__)
+
+  done = .FALSE.
+  do while (.not. done)
+
+     time = MPI_Wtime()
+
+     cl_status__ = run(cl_sweep_,3,cl_gwo__,cl_gws__,cl_lws__)
+     cl_status__ = clFinish(cl_sweep_%commands)
+     time_diff = time_sweep
+     time_sweep = time_sweep + MPI_Wtime() - time
+     time_diff = time_sweep - time_diff
+
+     cl_size__ = 4*newNX*newNY*newNZ
+     cl_status__ = readBuffer(cl_Changed_,C_LOC(Changed),cl_size__)
+     IF (sum(Changed) .le. 0) done = .TRUE.
+     cl_status__ = setKernelArg(cl_sweep_, 8, stepsTaken)
+     stepsTaken = stepsTaken + 1
+
+!!!     print *, "# changed:", sum(Changed), "step", stepsTaken, real(time_diff)
+     if (done .eq. .TRUE. .and. change .lt. 1) then
+        done = .FALSE.
+        change = change + 1
+     end if
+
+  end do  
+  totalSteps = totalSteps + stepsTaken
+  print *, "# sweeps and cumulative sweep time", stepsTaken, real(time_sweep)
+end do
+
+time_total = MPI_Wtime() - time0
+
+cl_size__   = 4*newNX*newNY*newNZ
 cl_status__ = readBuffer(cl_TT_,C_LOC(TT),cl_size__)
 
 print *, "------------------"
-print *, U(121,121,1:10)
+print *, U(i,j,1:10)
 print *, "------------------"
-print *, TT(121,121,1:10,1)
+print *, TT(i,j,1:10,1)
 print *, "------------------"
+
+PRINT *, ''
+PRINT *, "Sweep/reduce time for N=", nx,ny,nz, real(time_total), real(time_sweep)
+bandwidth = stepsTaken*4.0*nx*ny*nz*NFS*2.0 / (real(time_sweep) * 1000000000)
+PRINT *, "Steps Taken", totalSteps, "Bandwidth", bandwidth
 
 if (debug) then
   call write_results_padded(newNX,newNY,newNZ, nx,ny,nz, TT)
 end if  
-
-PRINT *, ''
-PRINT *, "Sweep/reduce time for N=", nx*ny*nz, real(time_sweep), real(time_reduce)
-bandwidth = stepsTaken*4.0*nx*ny*nz*NFS*2.0 / (real(time_sweep) * 1000000000)
-PRINT *, "Steps Taken", stepsTaken, "Bandwidth", bandwidth
 
 DEALLOCATE(U,TT,Changed,Offset,Dist)
 
